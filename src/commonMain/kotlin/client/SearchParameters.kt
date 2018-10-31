5356c2cec3c4bf2b3cc5f7a182fa26aa4daf3332
Quentin LITZLER <qlitzler@gmail.com>
2018-10-31 16:07:19 +0100

    feat: All search parameters

--- src/commonMain/kotlin/client/SearchParameters.kt
+++ src/commonMain/kotlin/client/SearchParameters.kt
@@ -41,14 +41,60 @@ data class SearchParameters(
     @Optional val minWordSizefor2Typos: Int? = null,
     @Optional val typoTolerance: String? = null,
     @Optional val allowTyposOnNumericTokens: Boolean? = null,
-    @Optional val disableTypoToleranceOnAttributes: List<String>? = null
+    @Optional val disableTypoToleranceOnAttributes: List<String>? = null,
     // Geo-Search
+    @Optional val aroundLatLng: String? = null,
+    @Optional val aroundLatLngViaIP: Boolean? = null,
+    @Optional val aroundRadius: AroundRadius? = null,
+    @Optional val aroundPrecision: Int? = null,
+    @Optional val minimumAroundRadius: Int? = null,
+    @Optional val insideBoundingBox: List<Float>? = null,
+    @Optional val insidePolygon: List<Float>? = null,
     // Languages
+    @Optional val ignorePlurals: BooleanOrISOCodes? = null,
+    @Optional val removeStopWords: BooleanOrISOCodes? = null,
+    @Optional val queryLanguages: List<String>? = null,
     // Query-rules
+    @Optional val enableRules: Boolean? = null,
+    @Optional val ruleContexts: List<String>? = null,
     // Query-Strategy
+    @Optional val queryType: String? = null,
+    @Optional val removeWordsIfNoResults: String? = null,
+    @Optional val advancedSyntax: Boolean? = null,
+    @Optional val optionalWords: List<String>? = null,
+    @Optional val disableExactOnAttributes: List<String>? = null,
+    @Optional val exactOnSingleWordQuery: String? = null,
+    @Optional val alternativesAsExact: List<String>? = null,
     // Advanced
-//    @Optional val analytics: Boolean = false
+    @Optional val distinct: Int? = null,
+    @Optional val getRankingInfo: Boolean? = null,
+    @Optional val clickAnalytics: Boolean? = null,
+    @Optional val analytics: Boolean? = null,
+    @Optional val analyticsTags: Boolean? = null,
+    @Optional val synonyms: Boolean? = null,
+    @Optional val replaceSynonymsInHighlight: Boolean? = null,
+    @Optional val minProximity: Int? = null,
+    @Optional val responseFields: List<String>? = null,
+    @Optional val maxFacetHits: Int? = null,
+    @Optional val percentileComputation: Boolean? = null
 ) {
+
+    sealed class AroundRadius {
+
+        object All : AroundRadius() {
+
+            const val parameter = "all"
+        }
+
+        data class InMeters(val int: kotlin.Int) : AroundRadius()
+    }
+
+    sealed class BooleanOrISOCodes {
+
+        class Boolean(val boolean: kotlin.Boolean) : BooleanOrISOCodes()
+
+        class ISOCodes(val codes: List<String>) : BooleanOrISOCodes()
+    }
 
     fun stringify(): String {
         val map = mutableMapOf<String, String>()
