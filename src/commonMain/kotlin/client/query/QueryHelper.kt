4ba6abff28ed2116dc6cf19d1040faec440a1acd
Quentin LITZLER <qlitzler@gmail.com>
2018-11-02 20:36:48 +0100

    feat: QueryHelper backup

---
+++ src/commonMain/kotlin/client/query/QueryHelper.kt
@@ -0,0 +1,19 @@
+package client.query
+
+
+fun queryBuilder(init: Query.() -> Unit) = Query().apply(init)
+
+fun Query.attributesToRetrieve(vararg attribute: String, excludeAttributes: Boolean = false) {
+    attributesToRetrieve = if (excludeAttributes) {
+        if (attribute.isNotEmpty()) {
+            attribute.map { "-$it" }.plus("*")
+        } else listOf()
+    } else attribute.toList()
+}
+
+fun example() {
+    val query = queryBuilder {
+        attributesToRetrieve("attributeA", "attributeB", excludeAttributes = true)
+    }
+    println(query.attributesToRetrieve) // ["-attributeA", "-attributeB", "*"]
+}
