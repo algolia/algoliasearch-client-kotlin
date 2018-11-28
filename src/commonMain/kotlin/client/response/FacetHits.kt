9730d6e8ee98b508672d6872e775b856d1134b1d
Quentin LITZLER <qlitzler@gmail.com>
2018-11-28 14:27:10 +0100

    fix: Non null facetHits for now

--- src/commonMain/kotlin/client/response/FacetHits.kt
+++ src/commonMain/kotlin/client/response/FacetHits.kt
@@ -1,20 +1,19 @@
 package client.response
 
-import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 
 
 @Serializable
 data class FacetHits(
-    @Optional val facetHits: List<Item>? = null,
-    @Optional val exhaustiveFacetsCount: Boolean? = null,
-    @Optional val processingTimeMS: Long? = null
+    val facetHits: List<Item>,
+    val exhaustiveFacetsCount: Boolean,
+    val processingTimeMS: Long
 ) {
 
     @Serializable
     data class Item(
-        @Optional val value: String? = null,
-        @Optional val highlighted: String? = null,
-        @Optional val count: Int? = null
+        val value: String,
+        val highlighted: String,
+        val count: Int
     )
 }
