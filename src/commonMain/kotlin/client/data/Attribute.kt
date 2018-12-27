9b0d2ee65a19fa370735127b8a1e54477257e6d6
Quentin LITZLER <qlitzler@gmail.com>
2018-12-27 23:09:51 +0100

    feat: serializer Attribute

--- src/commonMain/kotlin/client/data/Attribute.kt
+++ src/commonMain/kotlin/client/data/Attribute.kt
@@ -1,9 +1,27 @@
 package client.data
 
+import client.serialize.Serializer
+import client.serialize.unwrap
+import client.toAttribute
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.contentOrNull
+
 
 data class Attribute(val name: String) {
 
     override fun toString(): String {
         return name
+    }
+
+    internal companion object : Serializer<Attribute> {
+
+        override fun serialize(input: Attribute?): JsonElement {
+            return input.unwrap { JsonPrimitive(name) }
+        }
+
+        override fun deserialize(element: JsonElement): Attribute? {
+            return element.contentOrNull?.toAttribute()
+        }
     }
 }
--- src/jvmTest/kotlin/data/TestAttribute.kt
+++ src/jvmTest/kotlin/data/TestAttribute.kt
@@ -1,7 +1,11 @@
 package data
 
+import attributeA
+import attributeB
 import client.data.Attribute
 import client.toAttribute
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.jsonArray
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -9,7 +13,9 @@ import kotlin.test.assertEquals
 
 
 @RunWith(JUnit4::class)
-class TestAttribute {
+internal class TestAttribute : TestSerializer<Attribute> {
+
+    override val serializer = Attribute
 
     @Test
     fun dx() {
@@ -19,5 +25,37 @@ class TestAttribute {
     @Test
     fun raw() {
         assertEquals("raw", Attribute("raw").name)
+    }
+
+    @Test
+    override fun serialize() {
+        testSerialize(JsonPrimitive(attributeA.name), attributeA)
+        testSerializeNull()
+        testSerializeArray(
+            jsonArray {
+                +attributeA.name
+                +attributeB.name
+            },
+            listOf(
+                attributeA,
+                attributeB
+            )
+        )
+    }
+
+    @Test
+    override fun deserialize() {
+        testDeserialize(attributeA, JsonPrimitive(attributeA.name))
+        testDeserializeNull()
+        testDeserializeArray(
+            listOf(
+                attributeA,
+                attributeB
+            ),
+            jsonArray {
+                +attributeA.name
+                +attributeB.name
+            }
+        )
     }
 }
