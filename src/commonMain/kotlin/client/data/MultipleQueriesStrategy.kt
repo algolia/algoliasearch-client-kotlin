a69d7a7871e284f53b0581478209c74a325a9055
Quentin LITZLER <qlitzler@gmail.com>
2018-12-28 01:27:09 +0100

    feat: serializer MultipleQueriesStrategy

--- src/commonMain/kotlin/client/data/MultipleQueriesStrategy.kt
+++ src/commonMain/kotlin/client/data/MultipleQueriesStrategy.kt
@@ -2,6 +2,11 @@ package client.data
 
 import client.serialize.KeyNone
 import client.serialize.KeyStopIfEnoughMatches
+import client.serialize.Serializer
+import client.serialize.unwrap
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.contentOrNull
 
 
 sealed class MultipleQueriesStrategy(open val raw: String) {
@@ -11,4 +16,20 @@ sealed class MultipleQueriesStrategy(open val raw: String) {
     object StopIfEnoughMatches : MultipleQueriesStrategy(KeyStopIfEnoughMatches)
 
     data class Unknown(override val raw: String) : MultipleQueriesStrategy(raw)
+
+    internal companion object : Serializer<MultipleQueriesStrategy> {
+
+        override fun serialize(input: MultipleQueriesStrategy?): JsonElement {
+            return input.unwrap { JsonPrimitive(raw) }
+        }
+
+        override fun deserialize(element: JsonElement): MultipleQueriesStrategy? {
+            return when (val content = element.contentOrNull) {
+                KeyNone -> None
+                KeyStopIfEnoughMatches -> StopIfEnoughMatches
+                null -> null
+                else -> Unknown(content)
+            }
+        }
+    }
 }
---
+++ src/jvmTest/kotlin/serialize/TestMultipleQueriesStrategy.kt
@@ -0,0 +1,22 @@
+package serialize
+
+import client.data.MultipleQueriesStrategy
+import client.data.MultipleQueriesStrategy.*
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonPrimitive
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import tmp.TestSerializer
+import unknown
+
+
+@RunWith(JUnit4::class)
+internal class TestMultipleQueriesStrategy : TestSerializer<MultipleQueriesStrategy>(MultipleQueriesStrategy) {
+
+    override val item = listOf(
+        None to JsonPrimitive(None.raw),
+        StopIfEnoughMatches to JsonPrimitive(StopIfEnoughMatches.raw),
+        Unknown(unknown) to JsonPrimitive(unknown)
+    )
+    override val items: List<Pair<List<MultipleQueriesStrategy>, JsonArray>> = listOf()
+}
