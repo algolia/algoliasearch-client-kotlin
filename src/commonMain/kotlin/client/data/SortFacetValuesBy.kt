3513ce60ec645a65cdc248e0eced965e30580ddc
Quentin LITZLER <qlitzler@gmail.com>
2018-12-28 02:39:32 +0100

    feat: serializer SortFacetValuesBy

--- src/commonMain/kotlin/client/data/SortFacetValuesBy.kt
+++ src/commonMain/kotlin/client/data/SortFacetValuesBy.kt
@@ -2,6 +2,11 @@ package client.data
 
 import client.serialize.KeyAlpha
 import client.serialize.KeyCount
+import client.serialize.Serializer
+import client.serialize.unwrap
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.contentOrNull
 
 
 sealed class SortFacetValuesBy(open val raw: String) {
@@ -19,4 +24,20 @@ sealed class SortFacetValuesBy(open val raw: String) {
     object Alpha : SortFacetValuesBy(KeyAlpha)
 
     data class Unknown(override val raw: String) : SortFacetValuesBy(raw)
+
+    internal companion object : Serializer<SortFacetValuesBy> {
+
+        override fun serialize(input: SortFacetValuesBy?): JsonElement {
+            return input.unwrap { JsonPrimitive(raw) }
+        }
+
+        override fun deserialize(element: JsonElement): SortFacetValuesBy? {
+            return when (val content = element.contentOrNull) {
+                KeyCount -> Count
+                KeyAlpha -> Alpha
+                null -> null
+                else -> Unknown(content)
+            }
+        }
+    }
 }
---
+++ src/jvmTest/kotlin/serialize/TestSortFacetValuesBy.kt
@@ -0,0 +1,22 @@
+package serialize
+
+import client.data.SortFacetValuesBy
+import client.data.SortFacetValuesBy.*
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonPrimitive
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import tmp.TestSerializer
+import unknown
+
+
+@RunWith(JUnit4::class)
+internal class TestSortFacetValuesBy: TestSerializer<SortFacetValuesBy>(SortFacetValuesBy) {
+
+    override val item = listOf(
+        Alpha to JsonPrimitive(Alpha.raw),
+        Count to JsonPrimitive(Count.raw),
+        Unknown(unknown) to JsonPrimitive(unknown)
+    )
+    override val items: List<Pair<List<SortFacetValuesBy>, JsonArray>> = listOf()
+}
