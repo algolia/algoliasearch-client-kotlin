8717de29b805e046cfaad8cb99b7572b5b0a1bc5
Quentin LITZLER <qlitzler@gmail.com>
2018-12-28 01:53:59 +0100

    feat: serializer Polygon

--- src/commonMain/kotlin/client/data/Polygon.kt
+++ src/commonMain/kotlin/client/data/Polygon.kt
@@ -1,5 +1,11 @@
 package client.data
 
+import client.serialize.Serializer
+import client.serialize.unwrap
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.jsonArray
+
 
 data class Polygon(
     val point1: Point,
@@ -30,4 +36,42 @@ data class Polygon(
         *point3.floats.toTypedArray(),
         *points.flatMap { it.floats }.toTypedArray()
     )
+
+    internal companion object : Serializer<Polygon> {
+
+        override fun serialize(input: Polygon?): JsonElement {
+            return input.unwrap {
+                jsonArray {
+                    +(point1.latitude as Number)
+                    +(point1.longitude as Number)
+                    +(point2.latitude as Number)
+                    +(point2.longitude as Number)
+                    +(point3.latitude as Number)
+                    +(point3.longitude as Number)
+                    points.forEach {
+                        +(it.latitude as Number)
+                        +(it.longitude as Number)
+                    }
+                }
+            }
+        }
+
+        override fun deserialize(element: JsonElement): Polygon? {
+            return when (element) {
+                is JsonArray -> {
+                    val array = element.jsonArray
+
+                    Polygon(
+                        Point(array.getPrimitive(0).float, array.getPrimitive(1).float),
+                        Point(array.getPrimitive(2).float, array.getPrimitive(3).float),
+                        Point(array.getPrimitive(4).float, array.getPrimitive(5).float),
+                        (6 until array.size step 2).map {
+                            Point(array.getPrimitive(it).float, array.getPrimitive(it + 1).float)
+                        }
+                    )
+                }
+                else -> null
+            }
+        }
+    }
 }
---
+++ src/jvmTest/kotlin/serialize/TestPolygon.kt
@@ -0,0 +1,55 @@
+package serialize
+
+import client.data.Polygon
+import client.to
+import kotlinx.serialization.json.jsonArray
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import tmp.TestSerializer
+
+
+@RunWith(JUnit4::class)
+internal class TestPolygon : TestSerializer<Polygon>(Polygon) {
+
+    private val polygon = Polygon(
+        1f to 2f,
+        3f to 4f,
+        5f to 6f,
+        7f to 8f,
+        9f to 10f,
+        11f to 12f,
+        13f to 14f,
+        15f to 16f
+    )
+    private val jsonArray = jsonArray {
+        +(1f as Number)
+        +(2f as Number)
+        +(3f as Number)
+        +(4f as Number)
+        +(5f as Number)
+        +(6f as Number)
+        +(7f as Number)
+        +(8f as Number)
+        +(9f as Number)
+        +(10f as Number)
+        +(11f as Number)
+        +(12f as Number)
+        +(13f as Number)
+        +(14f as Number)
+        +(15f as Number)
+        +(16f as Number)
+    }
+
+    override val item = listOf(
+        polygon to jsonArray
+    )
+    override val items = listOf(
+        listOf(
+            polygon,
+            polygon
+        ) to jsonArray {
+            +jsonArray
+            +jsonArray
+        }
+    )
+}
