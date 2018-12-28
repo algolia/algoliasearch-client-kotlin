e027a970743f567ad3390e6299ce268834c73e70
Quentin LITZLER <qlitzler@gmail.com>
2018-12-28 18:03:47 +0100

    feat: settings serialization

--- src/commonMain/kotlin/client/response/Settings.kt
+++ src/commonMain/kotlin/client/response/Settings.kt
@@ -1,7 +1,10 @@
 package client.response
 
 import client.data.*
+import client.serialize.*
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.json
 
 
 @Serializable
@@ -14,7 +17,6 @@ data class Settings(
     var customRanking: List<CustomRanking>? = null,
     var replicas: List<Index>? = null,
     var maxValuesPerFacet: Int? = null,
-    var facetingAfterDistinct: Boolean? = null,
     var sortFacetValuesBy: SortFacetValuesBy? = null,
     var attributesToHighlight: List<Attribute>? = null,
     var attributesToSnippet: List<Snippet>? = null,
@@ -45,14 +47,86 @@ data class Settings(
     var disablePrefixOnAttributes: List<Attribute>? = null,
     var disableExactOnAttributes: List<Attribute>? = null,
     var exactOnSingleWordQuery: ExactOnSingleWordQuery? = null,
-    var alternativesAsExact: AlternativesAsExact? = null,
+    var alternativesAsExact: List<AlternativesAsExact>? = null,
     var numericAttributesForFiltering: List<NumericAttributeFilter>? = null,
     var allowCompressionOfIntegerArray: Boolean? = null,
     var attributeForDistinct: Attribute? = null,
     var distinct: Int? = null,
-    var synonyms: Boolean? = null,
     var replaceSynonymsInHighlight: Boolean? = null,
     var minProximity: Int? = null,
     var responseFields: List<ResponseFields>? = null,
     var maxFacetHits: Int? = null
-)
+) {
+
+    internal companion object : Serializer<Settings> {
+
+        override fun serialize(input: Settings): JsonElement {
+            return input.run {
+                json {
+                    // Attributes
+                    searchableAttributes?.let { KeySearchableAttributes to Attribute.serializeList(it) }
+                    attributesForFaceting?.let { KeyAttributesForFaceting to Attribute.serializeList(it) }
+                    unretrievableAttributes?.let { KeyUnretrievableAttributes to Attribute.serializeList(it) }
+                    attributesToRetrieve?.let { KeyAttributesToRetrieve to Attribute.serializeList(it) }
+                    // Ranking
+                    ranking?.let { KeyRanking to Ranking.serializeList(it) }
+                    customRanking?.let { KeyCustomRanking to CustomRanking.serializeList(it) }
+                    replicas?.let { KeyReplicas to Index.serializeList(it) }
+                    // Faceting
+                    maxValuesPerFacet?.let { KeyMaxValuesPerFacet to it }
+                    sortFacetValuesBy?.let { KeySortFacetValuesBy to SortFacetValuesBy.serialize(it) }
+                    // Highlighting
+                    attributesToHighlight?.let { KeyAttributesToHighlight to Attribute.serializeList(it) }
+                    attributesToSnippet?.let { KeyAttributesToSnippet to Snippet.serializeList(it) }
+                    highlightPreTag?.let { KeyHighlightPreTag to it }
+                    highlightPostTag?.let { KeyHighlightPostTag to it }
+                    snippetEllipsisText?.let { KeySnippetEllipsisText to it }
+                    restrictHighlightAndSnippetArrays?.let { KeyRestrictHighlightAndSnippetArrays to it }
+                    // Pagination
+                    hitsPerPage?.let { KeyHitsPerPage to it }
+                    paginationLimitedTo?.let { KeyPaginationLimitedTo to it }
+                    // Typos
+                    minWordSizefor1Typo?.let { KeyMinWordSizefor1Typo to it }
+                    minWordSizefor2Typos?.let { KeyMinWordSizefor2Typos to it }
+                    typoTolerance?.let { KeyTypoTolerance to TypoTolerance.serialize(it) }
+                    allowTyposOnNumericTokens?.let { KeyAllowTyposOnNumericTokens to it }
+                    disableTypoToleranceOnAttributes?.let {
+                        KeyDisableTypoToleranceOnAttributes to Attribute.serializeList(it)
+                    }
+                    disableTypoToleranceOnWords?.let { KeyDisableTypoToleranceOnWords to ListSerializer.serialize(it) }
+                    separatorsToIndex?.let { KeySeparatorsToIndex to it }
+                    // Languages
+                    ignorePlurals?.let { KeyIgnorePlurals to BooleanOrQueryLanguages.serialize(it) }
+                    removeStopWords?.let { KeyRemoveStopWords to BooleanOrQueryLanguages.serialize(it) }
+                    camelCaseAttributes?.let { KeyCamelCaseAttributes to Attribute.serializeList(it) }
+                    decompoundedAttributes?.let { KeyDecompoundedAttributes to DecompoundedAttributes.serializeList(it) }
+                    keepDiacriticsOnCharacters?.let { KeyKeepDiacriticsOnCharacters to it }
+                    queryLanguages?.let { KeyQueryLanguages to QueryLanguage.serializeList(it) }
+                    // Query-rules
+                    enableRules?.let { KeyEnableRules to it }
+                    // Query-strategy
+                    queryType?.let { KeyQueryType to QueryType.serialize(it) }
+                    removeWordsIfNoResults?.let { KeyRemoveWordsIfNoResults to RemoveWordIfNoResults.serialize(it) }
+                    advancedSyntax?.let { KeyAdvancedSyntax to it }
+                    optionalWords?.let { KeyOptionalWords to ListSerializer.serialize(it) }
+                    disablePrefixOnAttributes?.let { KeyDisablePrefixOnAttributes to Attribute.serializeList(it) }
+                    disableExactOnAttributes?.let { KeyDisableExactOnAttributes to Attribute.serializeList(it) }
+                    exactOnSingleWordQuery?.let { KeyExactOnSingleWordQuery to ExactOnSingleWordQuery.serialize(it) }
+                    alternativesAsExact?.let { KeyAlternativesAsExact to AlternativesAsExact.serializeList(it) }
+                    // Performance
+                    numericAttributesForFiltering?.let {
+                        KeyNumericAttributesForFiltering to NumericAttributeFilter.serializeList(it)
+                    }
+                    allowCompressionOfIntegerArray?.let { KeyAllowCompressionOfIntegerArray to it }
+                    // Advanced
+                    attributeForDistinct?.let { KeyAttributeForDistinct to Attribute.serialize(it) }
+                    distinct?.let { KeyDistinct to it }
+                    replaceSynonymsInHighlight?.let { KeyReplaceSynonymsInHighlight to it }
+                    minProximity?.let { KeyMinProximity to it }
+                    responseFields?.let { KeyResponseFields to ResponseFields.serializeList(it) }
+                    maxFacetHits?.let { KeyMaxFacetHits to it }
+                }
+            }
+        }
+    }
+}
---
+++ src/jvmTest/kotlin/serialize/TestSettings.kt
@@ -0,0 +1,150 @@
+package serialize
+
+import attributeA
+import attributes
+import boolean
+import client.data.*
+import client.response.Settings
+import client.serialize.*
+import indexA
+import int
+import jsonAttributes
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.json
+import kotlinx.serialization.json.jsonArray
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import string
+
+
+@RunWith(JUnit4::class)
+internal class TestSettings : TestSerializer<Settings>(Settings, null) {
+
+    override val item = listOf(
+        Settings(
+            // Attributes
+            searchableAttributes = attributes,
+            attributesForFaceting = attributes,
+            unretrievableAttributes = attributes,
+            attributesToRetrieve = attributes,
+            // Ranking
+            ranking = listOf(Ranking.Geo),
+            customRanking = listOf(CustomRanking.Asc(attributeA)),
+            replicas = listOf(indexA),
+            // Faceting
+            maxValuesPerFacet = int,
+            sortFacetValuesBy = SortFacetValuesBy.Count,
+            // Highlighting
+            attributesToHighlight = attributes,
+            attributesToSnippet = listOf(Snippet(attributeA)),
+            highlightPreTag = string,
+            highlightPostTag = string,
+            snippetEllipsisText = string,
+            restrictHighlightAndSnippetArrays = boolean,
+            // Pagination
+            hitsPerPage = int,
+            paginationLimitedTo = int,
+            minWordSizefor1Typo = int,
+            minWordSizefor2Typos = int,
+            // Typos
+            typoTolerance = TypoTolerance.Min,
+            allowTyposOnNumericTokens = boolean,
+            disableTypoToleranceOnAttributes = attributes,
+            disableTypoToleranceOnWords = listOf(string),
+            separatorsToIndex = string,
+            // Languages
+            ignorePlurals = BooleanOrQueryLanguages.Boolean(boolean),
+            removeStopWords = BooleanOrQueryLanguages.Boolean(boolean),
+            camelCaseAttributes = attributes,
+            decompoundedAttributes = listOf(DecompoundedAttributes(QueryLanguage.German, attributeA)),
+            keepDiacriticsOnCharacters = string,
+            queryLanguages = listOf(QueryLanguage.Afrikaans, QueryLanguage.Albanian),
+            // Query-rules
+            enableRules = boolean,
+            // Query-strategy
+            queryType = QueryType.PrefixLast,
+            removeWordsIfNoResults = RemoveWordIfNoResults.LastWords,
+            advancedSyntax = boolean,
+            optionalWords = listOf(string),
+            disablePrefixOnAttributes = attributes,
+            disableExactOnAttributes = attributes,
+            exactOnSingleWordQuery = ExactOnSingleWordQuery.Word,
+            alternativesAsExact = listOf(AlternativesAsExact.IgnorePlurals),
+            // Performance
+            numericAttributesForFiltering = listOf(NumericAttributeFilter(attributeA)),
+            allowCompressionOfIntegerArray = boolean,
+            // Advanced
+            attributeForDistinct = attributeA,
+            distinct = int,
+            replaceSynonymsInHighlight = boolean,
+            minProximity = int,
+            responseFields = listOf(ResponseFields.NbHits),
+            maxFacetHits = int
+        ) to json {
+            // Attributes
+            KeySearchableAttributes to jsonAttributes
+            KeyAttributesForFaceting to jsonAttributes
+            KeyUnretrievableAttributes to jsonAttributes
+            KeyAttributesToRetrieve to jsonAttributes
+            // Ranking
+            KeyRanking to jsonArray { +Ranking.Geo.raw }
+            KeyCustomRanking to jsonArray { +CustomRanking.Asc(attributeA).raw }
+            KeyReplicas to jsonArray { +indexA.raw }
+            // Faceting
+            KeyMaxValuesPerFacet to int
+            KeySortFacetValuesBy to SortFacetValuesBy.Count.raw
+            // Highlighting
+            KeyAttributesToHighlight to jsonAttributes
+            KeyAttributesToSnippet to jsonArray { +Snippet(attributeA).raw }
+            KeyHighlightPreTag to string
+            KeyHighlightPostTag to string
+            KeySnippetEllipsisText to string
+            KeyRestrictHighlightAndSnippetArrays to boolean
+            // Pagination
+            KeyHitsPerPage to int
+            KeyPaginationLimitedTo to int
+            KeyMinWordSizefor1Typo to int
+            KeyMinWordSizefor2Typos to int
+            // Typos
+            KeyTypoTolerance to TypoTolerance.Min.raw
+            KeyAllowTyposOnNumericTokens to boolean
+            KeyDisableTypoToleranceOnAttributes to jsonAttributes
+            KeyDisableTypoToleranceOnWords to jsonArray { +string }
+            KeySeparatorsToIndex to string
+            // Languages
+            KeyIgnorePlurals to boolean
+            KeyRemoveStopWords to boolean
+            KeyCamelCaseAttributes to jsonAttributes
+            KeyDecompoundedAttributes to DecompoundedAttributes.serializeList(
+                listOf(DecompoundedAttributes(QueryLanguage.German, attributeA))
+            )
+            KeyKeepDiacriticsOnCharacters to string
+            KeyQueryLanguages to jsonArray {
+                +QueryLanguage.Afrikaans.raw
+                +QueryLanguage.Albanian.raw
+            }
+            // Query-rules
+            KeyEnableRules to boolean
+            // Query-strategy
+            KeyQueryType to QueryType.PrefixLast.raw
+            KeyRemoveWordsIfNoResults to RemoveWordIfNoResults.LastWords.raw
+            KeyAdvancedSyntax to boolean
+            KeyOptionalWords to jsonArray { +string }
+            KeyDisablePrefixOnAttributes to jsonAttributes
+            KeyDisableExactOnAttributes to jsonAttributes
+            KeyExactOnSingleWordQuery to ExactOnSingleWordQuery.Word.raw
+            KeyAlternativesAsExact to jsonArray { +AlternativesAsExact.IgnorePlurals.raw }
+            // Performance
+            KeyNumericAttributesForFiltering to jsonArray { +NumericAttributeFilter(attributeA).raw }
+            KeyAllowCompressionOfIntegerArray to boolean
+            // Advanced
+            KeyAttributeForDistinct to attributeA.raw
+            KeyDistinct to int
+            KeyReplaceSynonymsInHighlight to boolean
+            KeyMinProximity to int
+            KeyResponseFields to jsonArray { +ResponseFields.NbHits.raw }
+            KeyMaxFacetHits to int
+        }
+    )
+    override val items: List<Pair<List<Settings>, JsonArray>> = listOf()
+}
