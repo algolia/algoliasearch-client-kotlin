f8d8564b21220573e9b251da7549a170c2057e30
Quentin LITZLER <qlitzler@gmail.com>
2018-12-31 16:32:13 +0100

    refac: Move Primitive to serialize package

---
+++ src/commonMain/kotlin/client/serialize/Primitive.kt
@@ -0,0 +1,64 @@
+package client.serialize
+
+import kotlinx.serialization.Decoder
+import kotlinx.serialization.Encoder
+import kotlinx.serialization.Serializer
+import kotlinx.serialization.json.*
+
+
+internal sealed class Primitive {
+
+    data class String(val raw: kotlin.String) : Primitive()
+
+    data class Number(val raw: kotlin.Number) : Primitive()
+
+    data class Boolean(val raw: kotlin.Boolean) : Primitive()
+
+    data class List(val raw: kotlin.collections.List<Primitive>) : Primitive()
+
+    data class Object(val raw: JsonObject) : Primitive()
+
+    object Null : Primitive()
+
+    @Serializer(Primitive::class)
+    companion object {
+
+        private fun deserialize(json: JsonElement): Primitive {
+            return when (json) {
+                is JsonNull -> Null
+                is JsonObject -> Object(json)
+                is JsonArray -> List(json.map(::deserialize))
+                is JsonLiteral -> {
+                    when {
+                        json.intOrNull != null -> Number(json.int)
+                        json.booleanOrNull != null -> Boolean(json.boolean)
+                        else -> String(json.content)
+                    }
+                }
+            }
+        }
+
+        private fun serialize(obj: Primitive): JsonElement {
+            return when (obj) {
+                is String -> JsonLiteral(obj.raw)
+                is Number -> JsonLiteral(obj.raw)
+                is Boolean -> JsonLiteral(obj.raw)
+                is List -> JsonArray(obj.raw.map(::serialize))
+                is Object -> obj.raw
+                is Null -> JsonNull
+            }
+        }
+
+        override fun serialize(output: Encoder, obj: Primitive) {
+            val json = output as JSON.JsonOutput
+
+            return json.writeTree(serialize(obj))
+        }
+
+        override fun deserialize(input: Decoder): Primitive {
+            val json = (input as JSON.JsonInput).readAsTree()
+
+            return deserialize(json)
+        }
+    }
+}
--- src/jvmTest/kotlin/serialize/TestPrimitive.kt
+++ src/jvmTest/kotlin/serialize/TestPrimitive.kt
@@ -1,77 +1,23 @@
-import kotlinx.serialization.Decoder
-import kotlinx.serialization.Encoder
+package serialize
+
+import client.serialize.Primitive
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Serializer
-import kotlinx.serialization.json.*
+import kotlinx.serialization.json.JSON
+import kotlinx.serialization.json.JsonLiteral
+import kotlinx.serialization.json.JsonObject
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import shouldEqual
 
-
-sealed class Primitive {
-
-    data class String(val raw: kotlin.String) : Primitive()
-
-    data class Number(val raw: kotlin.Number) : Primitive()
-
-    data class Boolean(val raw: kotlin.Boolean) : Primitive()
-
-    data class List(val raw: kotlin.collections.List<Primitive>) : Primitive()
-
-    data class Object(val raw: JsonObject) : Primitive()
-
-    object Null : Primitive()
-
-    @Serializer(Primitive::class)
-    companion object {
-
-        private fun deserialize(json: JsonElement): Primitive {
-            return when (json) {
-                is JsonNull -> Null
-                is JsonObject -> Object(json)
-                is JsonArray -> List(json.map(::deserialize))
-                is JsonLiteral -> {
-                    when {
-                        json.intOrNull != null -> Number(json.int)
-                        json.booleanOrNull != null -> Boolean(json.boolean)
-                        else -> String(json.content)
-                    }
-                }
-            }
-        }
-
-        private fun serialize(obj: Primitive): JsonElement {
-            return when (obj) {
-                is String -> JsonLiteral(obj.raw)
-                is Number -> JsonLiteral(obj.raw)
-                is Boolean -> JsonLiteral(obj.raw)
-                is List -> JsonArray(obj.raw.map(::serialize))
-                is Object -> obj.raw
-                is Null -> JsonNull
-            }
-        }
-
-        override fun serialize(output: Encoder, obj: Primitive) {
-            val json = output as JSON.JsonOutput
-
-            return json.writeTree(serialize(obj))
-        }
-
-        override fun deserialize(input: Decoder): Primitive {
-            val json = (input as JSON.JsonInput).readAsTree()
-
-            return deserialize(json)
-        }
-    }
-}
-
-@Serializable
-data class Obj(
-    @Serializable(Primitive.Companion::class) val primitive: Primitive
-)
 
 @RunWith(JUnit4::class)
-class Test {
+internal class TestPrimitive {
+
+    @Serializable
+    internal data class Obj(
+        @Serializable(Primitive.Companion::class) val primitive: Primitive
+    )
 
     private val serializer = Obj.serializer()
 
