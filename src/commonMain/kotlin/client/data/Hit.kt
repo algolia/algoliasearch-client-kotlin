1b3058089cc2e69971a26874569b1f05bd177e18
Quentin LITZLER <qlitzler@gmail.com>
2019-01-01 23:17:02 +0100

    feat: add json object

--- src/commonMain/kotlin/client/data/Hit.kt
+++ src/commonMain/kotlin/client/data/Hit.kt
@@ -5,17 +5,21 @@ import kotlinx.serialization.Decoder
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Serializer
+import kotlinx.serialization.json.JsonElement
 
 
 @Serializable
-data class Hit(val serialized: String) {
+data class Hit(
+    val serialized: String,
+    val element: JsonElement
+) {
 
     @Serializer(Hit::class)
     companion object : KSerializer<Hit> {
         override fun deserialize(input: Decoder): Hit {
             val json = input.readAsTree()
 
-            return Hit(json.toString())
+            return Hit(json.toString(), json)
         }
     }
 }
