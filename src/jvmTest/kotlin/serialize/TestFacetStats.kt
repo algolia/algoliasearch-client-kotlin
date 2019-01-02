94639d8a0f967f625286e519def9a3afaf9eb86e
Quentin LITZLER <qlitzler@gmail.com>
2019-01-02 22:21:05 +0100

    feat: TestFacetStats

---
+++ src/jvmTest/kotlin/serialize/TestFacetStats.kt
@@ -0,0 +1,21 @@
+package serialize
+
+import attributeA
+import attributeB
+import com.algolia.search.saas.data.Attribute
+import com.algolia.search.saas.data.FacetStats
+import com.algolia.search.saas.data.KSerializerFacetStats
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+
+@RunWith(JUnit4::class)
+internal class TestFacetStats : TestSerializer<Map<Attribute, FacetStats>>(KSerializerFacetStats) {
+
+    override val items = listOf(
+        mapOf(
+            attributeA to FacetStats(0, 1, 2f, 3f),
+            attributeB to FacetStats(1, 2, 3f, 4f)
+        )
+    )
+}
