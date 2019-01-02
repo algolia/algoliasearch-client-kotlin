6b320d7e8651e94b59f718763822893bae156072
Quentin LITZLER <qlitzler@gmail.com>
2019-01-02 02:07:40 +0100

    refac: remove search test debug

--- src/jvmTest/kotlin/client/TestClientSearch.kt
+++ src/jvmTest/kotlin/client/TestClientSearch.kt
@@ -16,10 +16,7 @@ internal class TestClientSearch {
     @Test
     fun search() {
         runBlocking {
-            val search = index.search(Query(facets = listOf(Attribute("color"), Attribute("price"))))
-
-            println(search)
-            println(search)
+            index.search()
         }
     }
 
