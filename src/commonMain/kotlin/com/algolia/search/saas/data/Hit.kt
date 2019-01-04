09b9b7610d96e9a5728191d8ab1fa3713c0e8667
Quentin LITZLER <qlitzler@gmail.com>
2019-01-04 11:06:21 +0100

    feat: hit deserialization

--- src/commonMain/kotlin/com/algolia/search/saas/data/Hit.kt
+++ src/commonMain/kotlin/com/algolia/search/saas/data/Hit.kt
@@ -1,13 +1,31 @@
 package com.algolia.search.saas.data
 
 import com.algolia.search.saas.serialize.KSerializerHighlights
-import kotlinx.serialization.Optional
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+import com.algolia.search.saas.serialize.asJsonInput
+import kotlinx.serialization.*
+import kotlinx.serialization.json.JSON
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.json
 
 
-@Serializable
+@Serializable(Hit.Companion::class)
 data class Hit(
-    @Optional @SerialName("_highlightResult") @Serializable(KSerializerHighlights::class)
+    @Transient val json: JsonElement = json { },
+    @Optional
+    @SerialName("_highlightResult")
+    @Serializable(KSerializerHighlights::class)
     val highlightResult: Map<Attribute, HighlightResult>? = null
-)
+) {
+
+    @Serializer(Hit::class)
+    companion object : KSerializer<Hit> {
+
+        override fun deserialize(input: Decoder): Hit {
+            val element = input.asJsonInput()
+            val highlightResult = element.jsonObject.getObjectOrNull("_highlightResult")
+            val parse = highlightResult?.let { JSON.nonstrict.parse(KSerializerHighlights, it.toString()) }
+
+            return Hit(element, parse)
+        }
+    }
+}
--- src/jvmTest/kotlin/serialize/TestHits.kt
+++ src/jvmTest/kotlin/serialize/TestHits.kt
@@ -3,8 +3,12 @@ package serialize
 import attributeA
 import attributeB
 import com.algolia.search.saas.data.*
+import com.algolia.search.saas.serialize.KSerializerHighlights
 import highlightResult
 import indexA
+import kotlinx.serialization.json.JSON
+import kotlinx.serialization.json.JsonTreeParser
+import kotlinx.serialization.json.json
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -13,11 +17,14 @@ import org.junit.runners.JUnit4
 internal class TestHits : TestSerializer<Hits>(Hits.serializer()) {
 
     private val highlights = mapOf(attributeA to highlightResult)
+    private val json = json {
+        "_highlightResult" to JsonTreeParser.parse(JSON.stringify(KSerializerHighlights, highlights))
+    }
 
     override val items = listOf(
         Hits(
             index = indexA,
-            hits = listOf(Hit(highlights)),
+            hits = listOf(Hit(json, highlights)),
             cursor = Cursor("cursor"),
             facets = mapOf(attributeA to listOf(Facet("facet", 0))),
             facetStats = mapOf(attributeB to FacetStats(0, 1, 2f, 3f)),
