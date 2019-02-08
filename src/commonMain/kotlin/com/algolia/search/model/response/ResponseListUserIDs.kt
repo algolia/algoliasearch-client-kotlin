50eabf17397b6683dff757892d90f30d0e65f55f
Quentin LITZLER <qlitzler@gmail.com>
2019-02-08 19:47:20 +0100

    add missing ResponseListUserIDs fields

--- src/commonMain/kotlin/com/algolia/search/model/response/ResponseListUserIDs.kt
+++ src/commonMain/kotlin/com/algolia/search/model/response/ResponseListUserIDs.kt
@@ -1,11 +1,17 @@
 package com.algolia.search.model.response
 
+import com.algolia.search.serialize.KeyHitsPerPage
+import com.algolia.search.serialize.KeyNbPages
+import com.algolia.search.serialize.KeyPage
 import com.algolia.search.serialize.KeyUserIDs
+import kotlinx.serialization.Optional
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 
 @Serializable
 data class ResponseListUserIDs(
-    @SerialName(KeyUserIDs) val userIDs: List<ResponseUserID>
+    @SerialName(KeyUserIDs) val userIDs: List<ResponseUserID>,
+    @Optional @SerialName(KeyPage) val page: Int? = null,
+    @Optional @SerialName(KeyHitsPerPage) val hitsPerPage: Int? = null
 )
