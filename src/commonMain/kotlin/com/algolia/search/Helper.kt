ff37fb971d98bb5ded619af590ae53e84a3af7ed
Quentin LITZLER <qlitzler@gmail.com>
2019-02-14 13:43:34 +0100

    Add browse methods

--- src/commonMain/kotlin/com/algolia/search/Helper.kt
+++ src/commonMain/kotlin/com/algolia/search/Helper.kt
@@ -1,9 +1,17 @@
 package com.algolia.search
 
+import com.algolia.search.client.Index
+import com.algolia.search.client.RequestOptions
 import com.algolia.search.model.*
 import com.algolia.search.model.enums.Point
 import com.algolia.search.model.enums.Snippet
+import com.algolia.search.model.queryrule.Anchoring
+import com.algolia.search.model.response.ResponseRules
+import com.algolia.search.model.response.ResponseSearch
+import com.algolia.search.model.response.ResponseSearchSynonyms
 import com.algolia.search.model.search.Cursor
+import com.algolia.search.model.search.Query
+import com.algolia.search.model.synonym.SynonymType
 import com.algolia.search.model.task.TaskID
 
 
@@ -49,4 +57,58 @@ infix fun Float.and(longitude: Float): Point {
 
 infix fun Attribute.limit(numberOfWords: Int?): Snippet {
     return Snippet(this, numberOfWords)
+}
+
+suspend fun Index.browseAllRules(
+    query: String? = null,
+    anchoring: Anchoring? = null,
+    context: String? = null,
+    hitsPerPage: Int? = null,
+    enabled: Boolean? = null,
+    requestOptions: RequestOptions? = null,
+    block: suspend ResponseRules.(Int) -> Unit
+) {
+    var page = 0
+
+    while (true) {
+        val response = searchRules(query, anchoring, context, page, hitsPerPage, enabled, requestOptions)
+
+        if (response.hits.isEmpty()) break
+        block(response, page++)
+    }
+}
+
+suspend fun Index.browseAllSynonyms(
+    query: String? = null,
+    hitsPerPage: Int? = null,
+    synonymType: List<SynonymType>? = null,
+    requestOptions: RequestOptions? = null,
+    block: suspend ResponseSearchSynonyms.(Int) -> Unit
+) {
+    var page = 0
+
+    while (true) {
+        val response = searchSynonyms(query, page, hitsPerPage, synonymType, requestOptions)
+
+        if (response.hits.isEmpty()) break
+        block(response, page++)
+    }
+}
+
+suspend fun Index.browseAllObjects(
+    query: Query? = null,
+    requestOptions: RequestOptions? = null,
+    block: suspend ResponseSearch.(Int) -> Unit
+) {
+    val initial = browse(query, requestOptions)
+    var cursor = initial.cursor
+    var page = 0
+
+    block(initial, page++)
+    while (cursor != null) {
+        val response = browse(cursor)
+
+        block(response, page++)
+        cursor = response.cursor
+    }
 }
---
+++ src/jvmTest/kotlin/suite/TestSuiteBrowse.kt
@@ -0,0 +1,100 @@
+package suite
+import com.algolia.search.browseAllObjects
+import com.algolia.search.browseAllRules
+import com.algolia.search.browseAllSynonyms
+import com.algolia.search.model.queryrule.QueryRule
+import com.algolia.search.model.search.Query
+import com.algolia.search.model.synonym.Synonym
+import com.algolia.search.model.task.TaskStatus
+import com.algolia.search.serialize.JsonNoNulls
+import com.algolia.search.toObjectID
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.json
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import shouldEqual
+
+
+@RunWith(JUnit4::class)
+internal class TestSuiteBrowse {
+
+    private val suffix = "helper"
+    private val indexName = testSuiteIndexName(suffix)
+    private val index = clientAdmin1.getIndex(indexName)
+
+    private fun loadQueryRule(name: String): QueryRule {
+        val string = loadScratch(name).readText()
+
+        println(string)
+        return JsonNoNulls.parse(QueryRule.serializer(), string)
+    }
+
+    @Before
+    fun clean() {
+        cleanIndex(clientAdmin1, suffix)
+    }
+
+    @Test
+    fun rules() {
+        runBlocking {
+            val ruleA = loadQueryRule("query_rule_brand.json")
+            val ruleB = loadQueryRule("query_rule_company.json")
+            var count = 0
+
+            index.apply {
+                saveRules(listOf(ruleA, ruleB)).wait() shouldEqual TaskStatus.Published
+
+                browseAllRules(hitsPerPage = 1) { page ->
+                    nbHits shouldEqual 2
+                    hits.size shouldEqual 1
+                    page shouldEqual count
+                    count++
+                }
+                count shouldEqual 2
+            }
+        }
+    }
+
+    @Test
+    fun synonyms() {
+        runBlocking {
+            val synonymA = Synonym.OneWay("a".toObjectID(), "a", listOf("b"))
+            val synonymB = Synonym.Placeholder("b".toObjectID(), Synonym.Placeholder.Token("as"), listOf("sad"))
+            var count = 0
+
+            index.apply {
+                saveSynonyms(listOf(synonymA, synonymB)).wait() shouldEqual TaskStatus.Published
+
+                browseAllSynonyms(hitsPerPage = 1) { page ->
+                    nbHits shouldEqual 2
+                    hits.size shouldEqual 1
+                    page shouldEqual count
+                    count++
+                }
+                count shouldEqual 2
+            }
+        }
+    }
+
+    @Test
+    fun objects() {
+        runBlocking {
+            val objects = (0 until 10).map { json { } }
+
+            index.apply {
+                saveObjects(objects).wait() shouldEqual TaskStatus.Published
+                var count = 0
+
+                browseAllObjects(Query(hitsPerPage = 1)) { page ->
+                    nbHits shouldEqual 10
+                    hits!!.size shouldEqual 1
+                    page shouldEqual count
+                    count++
+                }
+                count shouldEqual 10
+            }
+        }
+    }
+}
