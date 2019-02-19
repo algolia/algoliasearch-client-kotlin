3df653bdd86d9f7b38a0869af7faf2c0e02c315c
Quentin LITZLER <qlitzler@gmail.com>
2019-02-19 20:05:13 +0100

    Resolve ResponseListUserIDs todos

--- src/commonMain/kotlin/com/algolia/search/model/response/ResponseListUserIDs.kt
+++ src/commonMain/kotlin/com/algolia/search/model/response/ResponseListUserIDs.kt
@@ -3,24 +3,13 @@ package com.algolia.search.model.response
 import com.algolia.search.serialize.KeyHitsPerPage
 import com.algolia.search.serialize.KeyPage
 import com.algolia.search.serialize.KeyUserIDs
-import kotlinx.serialization.Optional
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.Transient
 
 
 @Serializable
 data class ResponseListUserIDs(
     @SerialName(KeyUserIDs) val userIDs: List<ResponseUserID>,
-    @Optional @SerialName(KeyPage) val pageOrNull: Int? = null, // TODO Check if actually nullable
-    @Optional @SerialName(KeyHitsPerPage) val hitsPerPageOrNull: Int? = null // TODO Check if actually nullable
-) {
-
-    @Transient
-    val page: Int
-        get() = pageOrNull!!
-
-    @Transient
-    val hitsPerPage: Int
-        get() = hitsPerPageOrNull!!
-}
+    @SerialName(KeyPage) val pageOrNull: Int,
+    @SerialName(KeyHitsPerPage) val hitsPerPageOrNull: Int
+)
