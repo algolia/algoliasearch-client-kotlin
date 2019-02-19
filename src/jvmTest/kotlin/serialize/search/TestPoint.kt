2f38a0f6fa2a71e59c6cb8a72fc0f3bf58754dc9
Quentin LITZLER <qlitzler@gmail.com>
2019-02-19 15:41:43 +0100

    Move serialize tests inside smaller packages

---
+++ src/jvmTest/kotlin/serialize/search/TestPoint.kt
@@ -0,0 +1,25 @@
+package serialize.search
+
+import com.algolia.search.model.search.Point
+import kotlinx.serialization.json.jsonArray
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import serialize.TestSerializer
+
+
+@RunWith(JUnit4::class)
+internal class TestPoint : TestSerializer<Point>(Point) {
+
+    override val items = listOf(
+        point to json
+    )
+
+    companion object {
+
+        val point = Point(1f, 2f)
+        val json = jsonArray {
+            +(1f as Number)
+            +(2f as Number)
+        }
+    }
+}
