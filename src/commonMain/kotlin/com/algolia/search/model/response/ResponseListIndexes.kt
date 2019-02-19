6c02523408cb5cabac0b098475657fd70987e4af
Quentin LITZLER <qlitzler@gmail.com>
2019-02-19 22:50:41 +0100

    Resolve ResponseListIndexes todos

--- src/commonMain/kotlin/com/algolia/search/model/response/ResponseListIndexes.kt
+++ src/commonMain/kotlin/com/algolia/search/model/response/ResponseListIndexes.kt
@@ -1,12 +1,12 @@
 package com.algolia.search.model.response
 
+import com.algolia.search.ClientDate
 import com.algolia.search.model.IndexName
 import com.algolia.search.serialize.*
 import kotlinx.serialization.Optional
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
-import kotlinx.serialization.json.JsonObject
 
 
 @Serializable
@@ -18,34 +18,35 @@ data class ResponseListIndexes(
     @Serializable
     data class Item(
         @SerialName(KeyName) val indexName: IndexName,
-        @SerialName(KeyCreatedAt) val createdAt: String,
-        @SerialName(KeyUpdatedAt) val updatedAt: String,
+        @SerialName(KeyCreatedAt) val createdAt: ClientDate,
+        @SerialName(KeyUpdatedAt) val updatedAt: ClientDate,
         @SerialName(KeyEntries) val entries: Int,
         @SerialName(KeyDataSize) val dataSize: Long,
         @SerialName(KeyFileSize) val fileSize: Long,
         @SerialName(KeyLastBuildTimeS) val lastBuildTimeS: Int,
         @SerialName(KeyNumberOfPendingTasks) val numberOfPendingTasks: Int,
         @SerialName(KeyPendingTask) val pendingTask: Boolean,
-        @Optional @SerialName(KeyReplicas) val replicasOrNull: List<String>? = null, // TODO Check if IndexName
-        @Optional @SerialName(KeyPrimary) val primaryOrNull: String? = null,
-        @Optional @SerialName(KeySourceABTest) val sourceABTestOrNull: String? = null, // TODO Check if IndexName
-        @Optional @SerialName(KeyABTest) val abTestOrNull: JsonObject? = null // TODO Check actual json format
+        @Optional @SerialName(KeyReplicas) val replicasOrNull: List<IndexName>? = null, // TODO Check if IndexName
+        @Optional @SerialName(KeyPrimary) val primaryOrNull: IndexName? = null,
+        @Optional @SerialName(KeySourceABTest) val sourceABTestOrNull: IndexName? = null, // TODO Check if IndexName
+        @Optional @SerialName(KeyABTest) val abTestOrNull: ResponseABTestShort? = null // TODO Check actual json format
     ) {
 
         @Transient
-        val replicas: List<String>
+        val replicas: List<IndexName>
             get() = replicasOrNull!!
 
         @Transient
-        val primary: String
+        val primary: IndexName
             get() = primaryOrNull!!
 
         @Transient
-        val sourceABTest: String
+        val sourceABTest: IndexName
             get() = sourceABTestOrNull!!
 
         @Transient
-        val abTest: JsonObject
+        val abTest: ResponseABTestShort
             get() = abTestOrNull!!
+
     }
 }
