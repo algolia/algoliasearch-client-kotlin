4405503697e8844ddf9653bb7a873daf50d66f85
Quentin LITZLER <qlitzler@gmail.com>
2019-02-22 17:45:09 +0100

    Fix TestSuiteMCM

--- src/jvmTest/kotlin/suite/TestSuiteMultiCluster.kt
+++ src/jvmTest/kotlin/suite/TestSuiteMultiCluster.kt
@@ -39,7 +39,7 @@ internal class TestSuiteMultiCluster {
                 }
                 delay(1000L)
             }
-            clientMcm.searchUserID(userID.raw).hits.any { it.userID == userID }.shouldBeTrue()
+            clientMcm.searchUserID(userID.raw).hits.shouldNotBeEmpty()
             clientMcm.listUserIDs().userIDs.shouldNotBeEmpty()
             clientMcm.getTopUserID().topUsers.shouldNotBeEmpty()
 
