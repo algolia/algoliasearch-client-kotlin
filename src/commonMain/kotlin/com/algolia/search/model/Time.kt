056bb5fce420292e78c9b1828439b0c95f647083
Quentin LITZLER <qlitzler@gmail.com>
2019-02-27 11:31:21 +0100

    Move time to model package

--- src/commonMain/kotlin/com/algolia/search/model/Time.kt
+++ src/commonMain/kotlin/com/algolia/search/model/Time.kt
@@ -1,4 +1,4 @@
-package com.algolia.search
+package com.algolia.search.model
 
 
 expect object Time {
--- src/jvmMain/kotlin/com/algolia/search/model/Time.kt
+++ src/jvmMain/kotlin/com/algolia/search/model/Time.kt
@@ -1,4 +1,4 @@
-package com.algolia.search
+package com.algolia.search.model
 
 import java.util.concurrent.TimeUnit
 
