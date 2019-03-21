2833d24ef4b981e0fa6df27e67ff40f22b6a8b36
Quentin LITZLER <qlitzler@gmail.com>
2019-03-21 16:57:27 +0100

    add serializable on RequestMultipleQueries

--- src/commonMain/kotlin/com/algolia/search/model/multipleindex/IndexQuery.kt
+++ src/commonMain/kotlin/com/algolia/search/model/multipleindex/IndexQuery.kt
@@ -2,8 +2,10 @@ package com.algolia.search.model.multipleindex
 
 import com.algolia.search.model.IndexName
 import com.algolia.search.model.search.Query
+import kotlinx.serialization.Serializable
 
 
+@Serializable
 public data class IndexQuery(
     val indexName: IndexName,
     val query: Query
--- src/commonMain/kotlin/com/algolia/search/model/request/RequestMultipleQueries.kt
+++ src/commonMain/kotlin/com/algolia/search/model/request/RequestMultipleQueries.kt
@@ -3,22 +3,19 @@ package com.algolia.search.model.request
 import com.algolia.search.model.multipleindex.IndexQuery
 import com.algolia.search.model.multipleindex.MultipleQueriesStrategy
 import com.algolia.search.serialize.*
-import kotlinx.serialization.Encoder
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.SerializationStrategy
-import kotlinx.serialization.internal.SerialClassDescImpl
+import kotlinx.serialization.*
 import kotlinx.serialization.json.json
 import kotlinx.serialization.json.jsonArray
 
 
+@Serializable(RequestMultipleQueries.Companion::class)
 internal class RequestMultipleQueries(
     @SerialName(KeyRequests) val indexQueries: List<IndexQuery>,
     @SerialName(KeyStrategy) val strategy: MultipleQueriesStrategy
 ) {
 
+    @Serializer(RequestMultipleQueries::class)
     internal companion object : SerializationStrategy<RequestMultipleQueries> {
-
-        override val descriptor = SerialClassDescImpl("RequestMultipleQueries")
 
         override fun serialize(encoder: Encoder, obj: RequestMultipleQueries) {
             val json = json {
