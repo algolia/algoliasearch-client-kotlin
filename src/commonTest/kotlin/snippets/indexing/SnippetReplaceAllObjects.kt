d9470e6ed720c43d0dfc95f3d51a04dd84f927a0
Quentin LITZLER <qlitzler@gmail.com>
2019-03-23 20:51:52 +0100

    SnippetReplaceAllObjects

---
+++ src/commonTest/kotlin/snippets/indexing/SnippetReplaceAllObjects.kt
@@ -0,0 +1,83 @@
+package snippets.indexing
+
+import clientAdmin1
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.json
+import runBlocking
+import suite.cleanIndex
+import suite.testSuiteIndexName
+import kotlin.test.AfterTest
+import kotlin.test.Test
+
+
+internal class SnippetReplaceAllObjects {
+
+//    suspend fun replaceAllObjects(
+//        data: __List<JsonObject>__
+//    ): List<Task>
+//
+//    suspend fun <T : Indexable> replaceAllObjects(
+//        serializer: KSerializer<T>,
+//        data: List<T>
+//    ): List<Task>
+
+    private val suffix = "snippet"
+    private val indexName = testSuiteIndexName(suffix)
+    private val client = clientAdmin1
+    private val index = client.initIndex(indexName)
+
+    @AfterTest
+    fun clean() {
+        runBlocking { cleanIndex(client, suffix) }
+    }
+
+    @Test
+    fun replaceAllObjects() {
+        runBlocking {
+            // With JsonObject
+            val json = listOf(
+                json {
+                    "firstname" to "Jimmie"
+                    "lastname" to "Barninger"
+                },
+                json {
+                    "firstname" to "Warren"
+                    "lastname" to "Speach"
+                }
+            )
+
+            index.replaceAllObjects(json)
+
+            // With serializable class
+            @Serializable
+            data class Person(val firstname: String, val lastname: String)
+
+            val persons = listOf(
+                Person("Jimmie", "Barninger"),
+                Person("Warren", "Speach")
+            )
+
+            index.replaceAllObjects(Person.serializer(), persons)
+        }
+    }
+
+    @Test
+    fun replaceAllObjectsWait() {
+        runBlocking {
+            client.apply {
+                val json = listOf(
+                    json {
+                        "firstname" to "Jimmie"
+                        "lastname" to "Barninger"
+                    },
+                    json {
+                        "firstname" to "Warren"
+                        "lastname" to "Speach"
+                    }
+                )
+
+                index.replaceAllObjects(json).waitAll()
+            }
+        }
+    }
+}
