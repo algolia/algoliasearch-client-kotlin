e3c0b68aa4a167f93ad7c73d42bfbe8fc2a32f02
Quentin LITZLER <qlitzler@gmail.com>
2019-03-23 19:09:05 +0100

    TestClientDateJVM

---
+++ src/jvmTest/kotlin/model/TestClientDateJVM.kt
@@ -0,0 +1,23 @@
+package model
+
+import com.algolia.search.helper.DateISO8601
+import com.algolia.search.model.ClientDate
+import org.junit.Test
+import shouldEqual
+import java.util.*
+
+
+internal class TestClientDateJVM {
+
+    private val timestamp = 10000L
+    private val format = DateISO8601.format(timestamp)
+    private val formatMillis = DateISO8601.format(timestamp, true)
+    private val date = Date(timestamp)
+
+    @Test
+    fun raw() {
+        ClientDate(format).date shouldEqual date
+        ClientDate(formatMillis).date shouldEqual date
+        ClientDate(timestamp).date shouldEqual date
+    }
+}
