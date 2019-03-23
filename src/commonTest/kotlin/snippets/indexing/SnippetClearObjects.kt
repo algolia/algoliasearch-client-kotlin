49fa7c6d6c7a9838125aa213fb42cd3d1f7686c0
Quentin LITZLER <qlitzler@gmail.com>
2019-03-23 22:33:03 +0100

    SnippetClearObjects

---
+++ src/commonTest/kotlin/snippets/indexing/SnippetClearObjects.kt
@@ -0,0 +1,35 @@
+package snippets.indexing
+
+import clientAdmin1
+import com.algolia.search.model.indexing.DeleteByQuery
+import com.algolia.search.model.search.AroundRadius
+import com.algolia.search.model.search.Point
+import runBlocking
+import suite.cleanIndex
+import suite.testSuiteIndexName
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+
+
+internal class SnippetClearObjects {
+
+//    suspend fun clearObjects(
+//        #{requestOptions}: __RequestOptions?__ = null
+//    ): RevisionIndex
+
+    private val suffix = "snippet"
+    private val indexName = testSuiteIndexName(suffix)
+    private val index = clientAdmin1.initIndex(indexName)
+
+    @BeforeTest
+    fun clean() {
+        runBlocking { cleanIndex(clientAdmin1, suffix) }
+    }
+
+    @Test
+    fun deleteObjectBy() {
+        runBlocking {
+            index.clearObjects()
+        }
+    }
+}
