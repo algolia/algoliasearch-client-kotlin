976a8e08ed8f721d45ad91f78d7a6b41656db601
Quentin LITZLER <qlitzler@gmail.com>
2019-03-23 20:22:24 +0100

    SnippetPartialUpdate

---
+++ src/commonTest/kotlin/snippets/indexing/SnippetPartialUpdate.kt
@@ -0,0 +1,68 @@
+package snippets.indexing
+
+import clientAdmin1
+import com.algolia.search.model.Attribute
+import com.algolia.search.model.ObjectID
+import com.algolia.search.model.indexing.Partial
+import runBlocking
+import suite.cleanIndex
+import suite.testSuiteIndexName
+import kotlin.test.AfterTest
+import kotlin.test.Test
+
+
+internal class SnippetPartialUpdate {
+
+//    suspend fun partialUpdateObject(
+//        objectID: __ObjectID__,
+//        partial: __Partial__,
+//        #{createIfNotExists}: __Boolean?__ = null,
+//        #{requestOptions}: __RequestOptions?__ = null
+//    ): RevisionObject
+//
+//    suspend fun partialUpdateObjects(
+//        data: __List<Pair<ObjectID, Partial>>__,
+//        #{createIfNotExists}: __Boolean__ = true,
+//        #{requestOptions}: __RequestOptions?__ = null
+//    ): ResponseBatch
+
+    private val suffix = "snippet"
+    private val indexName = testSuiteIndexName(suffix)
+    private val index = clientAdmin1.initIndex(indexName)
+
+    @AfterTest
+    fun clean() {
+        runBlocking { cleanIndex(clientAdmin1, suffix) }
+    }
+
+    @Test
+    fun partialUpdates() {
+        runBlocking {
+            val firstname = Attribute("firstname")
+            val partials = listOf(
+                ObjectID("myID1") to Partial.Update(firstname, "Jimmie"),
+                ObjectID("myID2") to Partial.Update(firstname, "Warren")
+            )
+
+            index.partialUpdateObjects(partials)
+        }
+    }
+
+    @Test
+    fun partialUpdate() {
+        runBlocking {
+            val partial = Partial.Update(Attribute("city"), "San Francisco")
+
+            index.partialUpdateObject(ObjectID("myID"), partial)
+        }
+    }
+
+    @Test
+    fun partialUpdateAdd() {
+        runBlocking {
+            val partial = Partial.Update(Attribute("state"), "California")
+
+            index.partialUpdateObject(ObjectID("myID"), partial)
+        }
+    }
+}
