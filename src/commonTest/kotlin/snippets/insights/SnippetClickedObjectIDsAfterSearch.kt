41ac6fad4de2e9dd79e013b0dbf899f8dfcfc9ab
Quentin LITZLER <qlitzler@gmail.com>
2019-03-24 21:27:44 +0100

    Snippet Insights

---
+++ src/commonTest/kotlin/snippets/insights/SnippetClickedObjectIDsAfterSearch.kt
@@ -0,0 +1,39 @@
+package snippets.insights
+
+import clientInsights
+import com.algolia.search.model.IndexName
+import com.algolia.search.model.ObjectID
+import com.algolia.search.model.QueryID
+import com.algolia.search.model.insights.EventName
+import com.algolia.search.model.insights.UserToken
+import runBlocking
+import snippets.TestSnippets
+import kotlin.test.Test
+
+
+class SnippetClickedObjectIDsAfterSearch : TestSnippets() {
+
+//    suspend fun ClientInsights.User.clickedObjectIDsAfterSearch(
+//        #{indexName}: __IndexName__,
+//        #{eventName}: __EventName__,
+//        #{queryID}: __QueryID__,
+//        #{objectIDs}: __List<ObjectID>__,
+//        #{positions}: __List<Int>__,
+//        timestamp: __Long?__ = null
+//    ): HttpResponse
+
+    @Test
+    fun clickedObjectIDsAfterSearch() {
+        runBlocking {
+            val userToken = UserToken("user-id")
+
+            clientInsights.User(userToken).clickedObjectIDsAfterSearch(
+                indexName = IndexName("indexName"),
+                eventName = EventName("eventName"),
+                objectIDs = listOf(ObjectID("objectID1"), ObjectID("objectID2")),
+                positions = listOf(17, 19),
+                queryID = QueryID("queryID")
+            )
+        }
+    }
+}
---
+++ src/commonTest/kotlin/snippets/insights/SnippetConvertedObjectIDs.kt
@@ -0,0 +1,33 @@
+package snippets.insights
+
+import clientInsights
+import com.algolia.search.model.IndexName
+import com.algolia.search.model.ObjectID
+import com.algolia.search.model.insights.EventName
+import com.algolia.search.model.insights.UserToken
+import runBlocking
+import kotlin.test.Test
+
+
+class SnippetConvertedObjectIDs {
+
+//    suspend fun ClientInsights.User.convertedObjectIDs(
+//        #{indexName}: __IndexName__,
+//        #{eventName}: __EventName__,
+//        #{objectIDs}: __List<ObjectID>__,
+//        timestamp: __Long?__ = null
+//    ): HttpResponse
+
+    @Test
+    fun convertedObjectIDs() {
+        runBlocking {
+            val userToken = UserToken("user-id")
+
+            clientInsights.User(userToken).convertedObjectIDs(
+                indexName = IndexName("indexName"),
+                eventName = EventName("eventName"),
+                objectIDs = listOf(ObjectID("objectID1"), ObjectID("objectID2"))
+            )
+        }
+    }
+}
