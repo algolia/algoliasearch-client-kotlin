01927d3aaa3d8bfdafee45cd331ce650e868f2d9
Quentin LITZLER <qlitzler@gmail.com>
2019-03-25 17:50:46 +0100

    Fix log test

--- src/commonTest/kotlin/suite/TestSuiteLogs.kt
+++ src/commonTest/kotlin/suite/TestSuiteLogs.kt
@@ -3,9 +3,7 @@ package suite
 import clientAdmin1
 import com.algolia.search.model.LogType
 import runBlocking
-import shouldBeEmpty
 import shouldEqual
-import snippets.index
 import kotlin.test.Test
 
 
@@ -17,8 +15,7 @@ internal class TestSuiteLogs {
             clientAdmin1.apply {
                 listIndices()
                 listIndices()
-                getLogs(page = 2, hitsPerPage = 0, logType = LogType.All).logs.size shouldEqual 2
-                index.getLogs().logs.shouldBeEmpty()
+                getLogs(hitsPerPage = 2, page = 0, logType = LogType.All).logs.size shouldEqual 2
             }
         }
     }
