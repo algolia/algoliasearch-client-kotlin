277391b9ab4bf3ca443234c2e1d3336fd8e18dab
Quentin LITZLER <qlitzler@gmail.com>
2019-04-05 15:28:54 +0200

    Add IP snippet in DocAnalytics

--- src/commonTest/kotlin/documentation/parameters/advanced/DocAnalytics.kt
+++ src/commonTest/kotlin/documentation/parameters/advanced/DocAnalytics.kt
@@ -1,6 +1,11 @@
 package documentation.parameters.advanced
 
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.configuration.ConfigurationSearch
 import com.algolia.search.dsl.query
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.IndexName
 import documentation.index
 import runBlocking
 import kotlin.test.Ignore
@@ -15,6 +20,28 @@ internal class DocAnalytics {
     @Test
     fun query() {
         runBlocking {
+            val query = query("query") {
+                analytics = true
+            }
+
+            index.search(query)
+        }
+    }
+
+    @Test
+    fun forwardIP() {
+        runBlocking {
+            // "94.228.178.246" should be replaced with your end user IP.
+            // Depending on your stack there are multiple ways to get this information.
+            val client = ClientSearch(
+                ConfigurationSearch(
+                    applicationID = ApplicationID("Your Application ID"),
+                    apiKey = APIKey("Your APP ID"),
+                    defaultHeaders = mapOf("X-Forwarded-For" to "94.228.178.246")
+                )
+            )
+            val index = client.initIndex(IndexName("index_name"))
+
             val query = query("query") {
                 analytics = true
             }
