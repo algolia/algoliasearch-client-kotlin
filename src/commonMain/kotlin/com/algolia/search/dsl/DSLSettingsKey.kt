c523352ae25ab0e5827698480d67aa4121f94aa3
Quentin LITZLER <qlitzler@gmail.com>
2019-04-06 19:19:18 +0200

    DSLSettingsKey

---
+++ src/commonMain/kotlin/com/algolia/search/dsl/DSLSettingsKey.kt
@@ -0,0 +1,69 @@
+package com.algolia.search.dsl
+
+import com.algolia.search.model.settings.SettingsKey
+
+
+@Suppress("PropertyName")
+public class DSLSettingsKey(
+    private val settingsKeys: MutableList<SettingsKey> = mutableListOf()
+) {
+    public val SearchableAttributes = SettingsKey.SearchableAttributes
+    public val AttributesForFaceting = SettingsKey.AttributesForFaceting
+    public val UnretrievableAttributes = SettingsKey.UnretrievableAttributes
+    public val AttributesToRetrieve = SettingsKey.AttributesToRetrieve
+    public val Ranking = SettingsKey.Ranking
+    public val CustomRanking = SettingsKey.CustomRanking
+    public val Replicas = SettingsKey.Replicas
+    public val MaxValuesPerFacet = SettingsKey.MaxValuesPerFacet
+    public val SortFacetsBy = SettingsKey.SortFacetsBy
+    public val AttributesToHighlight = SettingsKey.AttributesToHighlight
+    public val AttributesToSnippet = SettingsKey.AttributesToSnippet
+    public val HighlightPreTag = SettingsKey.HighlightPreTag
+    public val HighlightPostTag = SettingsKey.HighlightPostTag
+    public val SnippetEllipsisText = SettingsKey.SnippetEllipsisText
+    public val RestrictHighlightAndSnippetArrays = SettingsKey.RestrictHighlightAndSnippetArrays
+    public val HitsPerPage = SettingsKey.HitsPerPage
+    public val PaginationLimitedTo = SettingsKey.PaginationLimitedTo
+    public val MinWordSizefor1Typo = SettingsKey.MinWordSizefor1Typo
+    public val MinWordSizefor2Typos = SettingsKey.MinWordSizefor2Typos
+    public val TypoTolerance = SettingsKey.TypoTolerance
+    public val AllowTyposOnNumericTokens = SettingsKey.AllowTyposOnNumericTokens
+    public val DisableTypoToleranceOnAttributes = SettingsKey.DisableTypoToleranceOnAttributes
+    public val DisableTypoToleranceOnWords = SettingsKey.DisableTypoToleranceOnWords
+    public val SeparatorsToIndex = SettingsKey.SeparatorsToIndex
+    public val IgnorePlurals = SettingsKey.IgnorePlurals
+    public val RemoveStopWords = SettingsKey.RemoveStopWords
+    public val CamelCaseAttributes = SettingsKey.CamelCaseAttributes
+    public val DecompoundedAttributes = SettingsKey.DecompoundedAttributes
+    public val KeepDiacriticsOnCharacters = SettingsKey.KeepDiacriticsOnCharacters
+    public val QueryLanguages = SettingsKey.QueryLanguages
+    public val EnableRules = SettingsKey.EnableRules
+    public val QueryType = SettingsKey.QueryType
+    public val RemoveWordsIfNoResults = SettingsKey.RemoveWordsIfNoResults
+    public val AdvancedSyntax = SettingsKey.AdvancedSyntax
+    public val OptionalWords = SettingsKey.OptionalWords
+    public val DisablePrefixOnAttributes = SettingsKey.DisablePrefixOnAttributes
+    public val DisableExactOnAttributes = SettingsKey.DisableExactOnAttributes
+    public val ExactOnSingleWordQuery = SettingsKey.ExactOnSingleWordQuery
+    public val AlternativesAsExact = SettingsKey.AlternativesAsExact
+    public val NumericAttributesForFiltering = SettingsKey.NumericAttributesForFiltering
+    public val AllowCompressionOfIntegerArray = SettingsKey.AllowCompressionOfIntegerArray
+    public val AttributeForDistinct = SettingsKey.AttributeForDistinct
+    public val Distinct = SettingsKey.Distinct
+    public val ReplaceSynonymsInHighlight = SettingsKey.ReplaceSynonymsInHighlight
+    public val MinProximity = SettingsKey.MinProximity
+    public val ResponseFields = SettingsKey.ResponseFields
+    public val MaxFacetHits = SettingsKey.MaxFacetHits
+
+    public operator fun SettingsKey.unaryPlus() {
+        settingsKeys += this
+    }
+
+    public companion object :
+        DSL<DSLSettingsKey, List<SettingsKey>> {
+
+        override operator fun invoke(block: DSLSettingsKey.() -> Unit): List<SettingsKey> {
+            return DSLSettingsKey().apply(block).settingsKeys
+        }
+    }
+}
---
+++ src/commonTest/kotlin/dsl/TestDSLSettingsKey.kt
@@ -0,0 +1,72 @@
+package dsl
+
+import com.algolia.search.dsl.DSLSettingsKey
+import com.algolia.search.model.settings.SettingsKey
+import shouldEqual
+import kotlin.test.Test
+
+
+internal class TestDSLSettingsKey {
+
+    @Test
+    fun default() {
+        val dsl = DSLSettingsKey {
+            +AdvancedSyntax
+        }
+
+        dsl shouldEqual listOf(SettingsKey.AdvancedSyntax)
+    }
+
+    @Test
+    fun keys() {
+        DSLSettingsKey {
+            SearchableAttributes shouldEqual SettingsKey.SearchableAttributes
+            AttributesForFaceting shouldEqual SettingsKey.AttributesForFaceting
+            UnretrievableAttributes shouldEqual SettingsKey.UnretrievableAttributes
+            AttributesToRetrieve shouldEqual SettingsKey.AttributesToRetrieve
+            Ranking shouldEqual SettingsKey.Ranking
+            CustomRanking shouldEqual SettingsKey.CustomRanking
+            Replicas shouldEqual SettingsKey.Replicas
+            MaxValuesPerFacet shouldEqual SettingsKey.MaxValuesPerFacet
+            SortFacetsBy shouldEqual SettingsKey.SortFacetsBy
+            AttributesToHighlight shouldEqual SettingsKey.AttributesToHighlight
+            AttributesToSnippet shouldEqual SettingsKey.AttributesToSnippet
+            HighlightPreTag shouldEqual SettingsKey.HighlightPreTag
+            HighlightPostTag shouldEqual SettingsKey.HighlightPostTag
+            SnippetEllipsisText shouldEqual SettingsKey.SnippetEllipsisText
+            RestrictHighlightAndSnippetArrays shouldEqual SettingsKey.RestrictHighlightAndSnippetArrays
+            HitsPerPage shouldEqual SettingsKey.HitsPerPage
+            PaginationLimitedTo shouldEqual SettingsKey.PaginationLimitedTo
+            MinWordSizefor1Typo shouldEqual SettingsKey.MinWordSizefor1Typo
+            MinWordSizefor2Typos shouldEqual SettingsKey.MinWordSizefor2Typos
+            TypoTolerance shouldEqual SettingsKey.TypoTolerance
+            AllowTyposOnNumericTokens shouldEqual SettingsKey.AllowTyposOnNumericTokens
+            DisableTypoToleranceOnAttributes shouldEqual SettingsKey.DisableTypoToleranceOnAttributes
+            DisableTypoToleranceOnWords shouldEqual SettingsKey.DisableTypoToleranceOnWords
+            SeparatorsToIndex shouldEqual SettingsKey.SeparatorsToIndex
+            IgnorePlurals shouldEqual SettingsKey.IgnorePlurals
+            RemoveStopWords shouldEqual SettingsKey.RemoveStopWords
+            CamelCaseAttributes shouldEqual SettingsKey.CamelCaseAttributes
+            DecompoundedAttributes shouldEqual SettingsKey.DecompoundedAttributes
+            KeepDiacriticsOnCharacters shouldEqual SettingsKey.KeepDiacriticsOnCharacters
+            QueryLanguages shouldEqual SettingsKey.QueryLanguages
+            EnableRules shouldEqual SettingsKey.EnableRules
+            QueryType shouldEqual SettingsKey.QueryType
+            RemoveWordsIfNoResults shouldEqual SettingsKey.RemoveWordsIfNoResults
+            AdvancedSyntax shouldEqual SettingsKey.AdvancedSyntax
+            OptionalWords shouldEqual SettingsKey.OptionalWords
+            DisablePrefixOnAttributes shouldEqual SettingsKey.DisablePrefixOnAttributes
+            DisableExactOnAttributes shouldEqual SettingsKey.DisableExactOnAttributes
+            ExactOnSingleWordQuery shouldEqual SettingsKey.ExactOnSingleWordQuery
+            AlternativesAsExact shouldEqual SettingsKey.AlternativesAsExact
+            NumericAttributesForFiltering shouldEqual SettingsKey.NumericAttributesForFiltering
+            AllowCompressionOfIntegerArray shouldEqual SettingsKey.AllowCompressionOfIntegerArray
+            AttributeForDistinct shouldEqual SettingsKey.AttributeForDistinct
+            Distinct shouldEqual SettingsKey.Distinct
+            ReplaceSynonymsInHighlight shouldEqual SettingsKey.ReplaceSynonymsInHighlight
+            MinProximity shouldEqual SettingsKey.MinProximity
+            ResponseFields shouldEqual SettingsKey.ResponseFields
+            MaxFacetHits shouldEqual SettingsKey.MaxFacetHits
+        }
+    }
+}
