1bf9b5f2a914f1e9955b064c210f19625c34f89f
Quentin LITZLER <qlitzler@gmail.com>
2019-04-06 13:08:21 +0200

    Multi cluster snippet

---
+++ src/commonTest/kotlin/documentation/DocMultiCluster.kt
@@ -0,0 +1,32 @@
+package documentation
+
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.configuration.ConfigurationSearch
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.IndexName
+import com.algolia.search.model.search.Query
+import runBlocking
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+
+@Ignore
+internal class DocMultiCluster {
+
+    @Test
+    fun snippet() {
+        runBlocking {
+            val client = ClientSearch(
+                ConfigurationSearch(
+                    applicationID = ApplicationID("Your Application ID"),
+                    apiKey = APIKey("Your APP ID"),
+                    defaultHeaders = mapOf("X-Algolia-User-ID" to "user123")
+                )
+            )
+            val index = client.initIndex(IndexName("index_name"))
+
+            index.search(Query("query"))
+        }
+    }
+}
