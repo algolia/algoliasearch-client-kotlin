80c7e0a89213b632c31746aa984f61bf9c5de875
Quentin LITZLER <qlitzler@gmail.com>
2019-04-08 18:20:43 +0200

    gradle buildSrc

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,5 @@
 import java.net.URI
 
-object Version {
-
-    const val kotlin = "1.3.30-eap-42"
-    const val ktor = "1.2.0-alpha-2"
-    const val serialization = "0.11.0-1.3.30-eap-42"
-}
 
 plugins {
     id("kotlin-multiplatform") version "1.3.30-eap-45"
---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,5 @@
+object Version {
+
+    const val ktor = "1.2.0-alpha-2"
+    const val serialization = "0.11.0-1.3.30-eap-42"
+}
