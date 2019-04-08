71042eda1b2211e22231ea0caabe1e263380f3f7
Quentin LITZLER <qlitzler@gmail.com>
2019-04-08 16:40:49 +0200

    Markting Snippet

---
+++ src/commonTest/kotlin/documentation/marketing/MarketingSnippet.kt
@@ -0,0 +1,25 @@
+package documentation.marketing
+
+import com.algolia.search.model.IndexName
+import documentation.client
+import kotlinx.serialization.json.json
+import runBlocking
+import kotlin.test.Test
+
+
+internal class MarketingSnippet {
+
+    @Test
+    fun snippet1() {
+        runBlocking {
+            val index = client.initIndex(IndexName("contacts"))
+            val json = json {
+                "firstname" to "Jimmie"
+                "lastname" to "Barninger"
+                "company" to "California Paint"
+            }
+
+            index.saveObject(json)
+        }
+    }
+}
