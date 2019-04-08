76e1578ca0c1d1b12d6012eec42df07734003d95
Quentin LITZLER <qlitzler@gmail.com>
2019-04-08 16:40:44 +0200

    Tutorial snippets

---
+++ src/jvmTest/kotlin/documentation/tutorials/TutorialPushData.kt
@@ -0,0 +1,82 @@
+package documentation.tutorials
+
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.IndexName
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObjectSerializer
+import kotlinx.serialization.list
+import org.junit.Test
+
+
+internal class TutorialPushData {
+
+//    // In your build.gradle
+//    dependencies {
+//        implementation "com.algolia:algoliasearch-client-kotlin-jvm:$kotlin_client_version"
+//    }
+
+    @Test
+    fun ecommerce() {
+        runBlocking {
+            val content = HttpClient().get<String>("https://alg.li/doc-ecommerce.json")
+            val objects = Json.parse(JsonObjectSerializer.list, content)
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAdminAPIKey")
+            )
+
+            val index = client.initIndex(IndexName("demo_ecommerce"))
+            index.saveObjects(objects)
+        }
+    }
+
+    @Test
+    fun saas() {
+        runBlocking {
+            val content = HttpClient().get<String>("https://alg.li/doc-saas.json")
+            val objects = Json.parse(JsonObjectSerializer.list, content)
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAdminAPIKey")
+            )
+
+            val index = client.initIndex(IndexName("demo_saas"))
+            index.saveObjects(objects)
+        }
+    }
+
+    @Test
+    fun media() {
+        runBlocking {
+            val content = HttpClient().get<String>("https://alg.li/doc-media.json")
+            val objects = Json.parse(JsonObjectSerializer.list, content)
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAdminAPIKey")
+            )
+
+            val index = client.initIndex(IndexName("demo_media"))
+            index.saveObjects(objects)
+        }
+    }
+
+    @Test
+    fun geo() {
+        runBlocking {
+            val content = HttpClient().get<String>("https://alg.li/doc-geo.json")
+            val objects = Json.parse(JsonObjectSerializer.list, content)
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAdminAPIKey")
+            )
+
+            val index = client.initIndex(IndexName("demo_geo"))
+            index.saveObjects(objects)
+        }
+    }
+}
---
+++ src/jvmTest/kotlin/documentation/tutorials/TutorialSetRelevance.kt
@@ -0,0 +1,165 @@
+package documentation.tutorials
+
+import com.algolia.search.client.ClientSearch
+import com.algolia.search.dsl.*
+import com.algolia.search.dsl.rule.rules
+import com.algolia.search.model.APIKey
+import com.algolia.search.model.ApplicationID
+import com.algolia.search.model.Attribute
+import com.algolia.search.model.IndexName
+import com.algolia.search.model.rule.Condition
+import com.algolia.search.model.rule.Consequence
+import com.algolia.search.model.search.AlternativesAsExact
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+
+internal class TutorialSetRelevance {
+
+    @Test
+    fun ecommerce() {
+        runBlocking {
+            val settings = settings {
+                // Select the attributes you want to search in
+                searchableAttributes {
+                    +"brand"
+                    +"name"
+                    +"categories"
+                    +"description"
+                }
+                // Define business metrics for ranking and sorting
+                customRanking {
+                    +Desc("popularity")
+                }
+                // Set up some attributes to filter results on
+                attributesForFaceting {
+                    +"categories"
+                    +Searchable("brand")
+                    +"price"
+                }
+            }
+
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAPIKey")
+            )
+            val index = client.initIndex(IndexName("demo_ecommerce"))
+
+            index.setSettings(settings)
+        }
+    }
+
+    @Test
+    fun saas() {
+        runBlocking {
+            val settings = settings {
+                // Select the attributes you want to search in
+                searchableAttributes {
+                    +"Name"
+                    +"Owner"
+                    +"Account"
+                    +"Email"
+                    +"Website"
+                }
+                // Define business metrics for ranking and sorting
+                customRanking {
+                    +Desc("Amount")
+                }
+                // Set up some attributes to filter results on
+                attributesForFaceting {
+                    +"type"
+                }
+                attributeForDistinct = Attribute("type")
+            }
+
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAPIKey")
+            )
+            val index = client.initIndex(IndexName("demo_saas"))
+
+            index.setSettings(settings)
+        }
+    }
+
+    @Test
+    fun media() {
+        runBlocking {
+            val settings = settings {
+                // Select the attributes you want to search in
+                searchableAttributes {
+                    +"post_title"
+                    +"author_name"
+                    +"categories"
+                    +"content"
+                }
+                // Define business metrics for ranking and sorting
+                customRanking {
+                    +Desc("post_date")
+                    +Desc("record_index")
+                }
+                // Set up some attributes to filter results on
+                attributesForFaceting {
+                    +"categories"
+                }
+                // Define the attribute we want to distinct on
+                attributeForDistinct = Attribute("post_id")
+            }
+
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAPIKey")
+            )
+            val index = client.initIndex(IndexName("demo_media"))
+
+            index.setSettings(settings)
+        }
+    }
+
+    @Test
+    fun geo() {
+        runBlocking {
+            val settings = settings {
+                searchableAttributes {
+                    +"country"
+                    +"city"
+                    +"name"
+                    +"airport_id"
+                }
+                customRanking {
+                    +Desc("nb_airline_liaisons")
+                }
+                attributesForFaceting {
+                    +"city"
+                    +"country"
+                }
+            }
+
+            AlternativesAsExact
+
+            val queryRules = rules {
+                rule(
+                    objectID = "country",
+                    condition = Condition(Contains, Facet("country")),
+                    consequence = Consequence(query = query { aroundLatLngViaIP = false })
+                )
+                rule(
+                    objectID = "city",
+                    condition = Condition(Contains, Facet("city")),
+                    consequence = Consequence(query = query { aroundLatLngViaIP = false })
+                )
+            }
+
+            val client = ClientSearch(
+                ApplicationID("YourApplicationID"),
+                APIKey("YourAPIKey")
+            )
+            val index = client.initIndex(IndexName("demo_geo"))
+
+            // Configure relevancy
+            index.setSettings(settings)
+            // Add special rules
+            index.saveRules(queryRules)
+        }
+    }
+}
