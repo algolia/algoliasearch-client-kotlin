021db7020a1318b28e539a842ebd3dea22ad2af5
Quentin LITZLER <qlitzler@gmail.com>
2019-06-15 13:58:14 +0200

    GeoPoint serializer

---
+++ src/commonTest/kotlin/serialize/serializer/TestKSerializerGeoPoint.kt
@@ -0,0 +1,25 @@
+package serialize.serializer
+
+import com.algolia.search.model.search.Point
+import com.algolia.search.serialize.KSerializerGeoPoint
+import com.algolia.search.serialize.KeyLat
+import com.algolia.search.serialize.KeyLng
+import kotlinx.serialization.json.json
+import serialize.TestSerializer
+
+
+internal class TestKSerializerGeoPoint : TestSerializer<Point>(KSerializerGeoPoint) {
+
+    override val items = listOf(
+        point to json
+    )
+
+    companion object {
+
+        val json = json {
+            KeyLat to 0f
+            KeyLng to 1f
+        }
+        val point = Point(latitude = 0f, longitude = 1f)
+    }
+}
