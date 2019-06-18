cbd1ca93deb6fe0ee31a839d6b3ecd73af015998
Quentin LITZLER <qlitzler@gmail.com>
2019-06-18 14:24:52 +0200

    DSLPlaces

---
+++ src/commonTest/kotlin/dsl/TestDSLPlacesQuery.kt
@@ -0,0 +1,21 @@
+package dsl
+
+import com.algolia.search.dsl.countries
+import com.algolia.search.dsl.placesQuery
+import shouldNotBeNull
+import kotlin.test.Test
+
+
+internal class TestDSLPlacesQuery {
+
+    @Test
+    fun countries() {
+        val placesQuery = placesQuery {
+            countries {
+                +France
+            }
+        }
+
+        placesQuery.countries.shouldNotBeNull()
+    }
+}
