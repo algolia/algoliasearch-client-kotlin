9af84d4f9e99b0bbb8d1ff90b21c5c7a18298cb0
Quentin LITZLER <qlitzler@gmail.com>
2019-07-02 17:48:56 +0200

    Increase default read timeout

--- src/commonMain/kotlin/com/algolia/search/configuration/Extensions.kt
+++ src/commonMain/kotlin/com/algolia/search/configuration/Extensions.kt
@@ -4,5 +4,5 @@ import io.ktor.client.features.logging.LogLevel
 
 
 internal const val defaultWriteTimeout: Long = 30000L
-internal const val defaultReadTimeout: Long = 2000L
+internal const val defaultReadTimeout: Long = 5000L
 internal val defaultLogLevel = LogLevel.NONE
