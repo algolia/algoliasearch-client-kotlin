652a4e7428a389605134593d0e69bdb999366563
Quentin LITZLER <qlitzler@gmail.com>
2019-07-08 17:16:28 +0200

    Add EmptyBody string constant

---
+++ src/commonTest/kotlin/model/request/TestRequestEmptyBody.kt
@@ -0,0 +1,14 @@
+package model.request
+
+import com.algolia.search.model.request.EmptyBody
+import shouldEqual
+import kotlin.test.Test
+
+
+internal class TestRequestEmptyBody {
+
+    @Test
+    fun test() {
+        EmptyBody shouldEqual ""
+    }
+}
