216ea0bfbc09a8de6966bff4b7bb8568bc84593a
Quentin LITZLER <qlitzler@gmail.com>
2019-08-21 11:54:46 +0200

    Update GuideHideHits

--- src/commonTest/kotlin/documentation/guides/results/promoting/GuideHideHits.kt
+++ src/commonTest/kotlin/documentation/guides/results/promoting/GuideHideHits.kt
@@ -1,9 +1,13 @@
 package documentation.guides.results.promoting
 
+import com.algolia.search.dsl.filters
 import com.algolia.search.dsl.objectIDs
+import com.algolia.search.dsl.query
 import com.algolia.search.dsl.rule.rules
+import com.algolia.search.model.rule.Anchoring
 import com.algolia.search.model.rule.Condition
 import com.algolia.search.model.rule.Consequence
+import com.algolia.search.model.rule.Pattern
 import documentation.index
 import runBlocking
 import kotlin.test.Ignore
@@ -18,9 +22,33 @@ internal class GuideHideHits {
         runBlocking {
             val rules = rules {
                 rule(
-                    "hide-12345",
-                    Condition(Contains, Literal("harry potter")),
-                    Consequence(hide = objectIDs { +"HP-12345" })
+                    objectID = "hide-12345",
+                    condition = Condition(Contains, Literal("harry potter")),
+                    consequence = Consequence(hide = objectIDs { +"HP-12345" })
+                )
+            }
+
+            index.saveRules(rules)
+        }
+    }
+
+    @Test
+    fun snippet2() {
+        runBlocking {
+            val rules = rules {
+                rule(
+                    objectID = "hide-shirts",
+                    condition = Condition(
+                        pattern = Pattern.Literal("banana"),
+                        anchoring = Anchoring.Is
+                    ),
+                    consequence = Consequence(
+                        query = query {
+                            filters {
+                                and { facet("clothing-type", "t-shirts", isNegated = true) }
+                            }
+                        }
+                    )
                 )
             }
 
