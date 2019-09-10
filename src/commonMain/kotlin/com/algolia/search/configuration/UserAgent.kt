f5f3f171b70df724d00c807108180a2f3fefd3b4
Paul-Louis NECH <pln@algolia.com>
2019-09-10 17:08:55 +0200

    refactor(UserAgent): improve JavaDX using JvmName

--- src/commonMain/kotlin/com/algolia/search/configuration/UserAgent.kt
+++ src/commonMain/kotlin/com/algolia/search/configuration/UserAgent.kt
@@ -1,4 +1,8 @@
+@file:JvmName("UserAgentUtils")
+
 package com.algolia.search.configuration
 
+import kotlin.jvm.JvmName
 
+@JvmName("client")
 public fun clientUserAgent(version: String) = "Algolia for Kotlin ($version)"
