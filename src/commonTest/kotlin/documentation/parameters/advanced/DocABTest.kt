e98b9177f3fff36d3d6bfa9470a06ec41baffb58
Quentin LITZLER <qlitzler@gmail.com>
2019-11-06 10:57:25 +0100

    Add DocABTest

---
+++ src/commonTest/kotlin/documentation/parameters/advanced/DocABTest.kt
@@ -0,0 +1,24 @@
+package documentation.parameters.advanced
+
+import com.algolia.search.dsl.query
+import documentation.index
+import runBlocking
+import kotlin.test.Ignore
+import kotlin.test.Test
+
+
+@Ignore
+class DocABTest {
+
+//    enableABTest: Boolean = true|false
+
+    @Test
+    fun snippet1() {
+        runBlocking {
+            val query = query("query") {
+                enableABTest = false
+            }
+            index.search(query)
+        }
+    }
+}
