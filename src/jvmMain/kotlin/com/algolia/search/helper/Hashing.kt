6a69427ae8480182160145fce16b37124fe568e1
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-04-01 16:26:17 +0200

    feat(ktor): create and use internal HexConverter

--- src/jvmMain/kotlin/com/algolia/search/helper/Hashing.kt
+++ src/jvmMain/kotlin/com/algolia/search/helper/Hashing.kt
@@ -4,18 +4,14 @@ import io.ktor.utils.io.core.toByteArray
 import java.util.Base64
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
-import kotlinx.serialization.InternalSerializationApi
-import kotlinx.serialization.internal.HexConverter
 
-// TODO: Remove deprecated HexConverter object
-@OptIn(InternalSerializationApi::class)
 internal actual fun String.sha256(key: String): String {
     return Mac.getInstance("HmacSHA256").run {
         val secretKey = SecretKeySpec(this@sha256.toByteArray(), "HmacSHA256")
 
         init(secretKey)
         val hash = doFinal(key.toByteArray())
-        HexConverter.printHexBinary(hash, true)
+        hash.toHex(true)
     }
 }
 
