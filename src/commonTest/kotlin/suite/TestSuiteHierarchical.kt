13b18c990d16920f16e9821d3620137522cbb3a8
Guy Daher <guydaher1@gmail.com>
2020-04-01 11:47:37 +0200

    feat(ktor): fix testHierarchical ordering issue

--- src/commonTest/kotlin/suite/TestSuiteHierarchical.kt
+++ src/commonTest/kotlin/suite/TestSuiteHierarchical.kt
@@ -140,8 +140,8 @@ internal class TestSuiteHierarchical {
             response.hierarchicalFacets shouldEqual mapOf(
                 hierarchicalCategoryLvl0 to listOf(Facet(category3, 6), Facet(category2, 2), Facet(category1, 1)),
                 hierarchicalCategoryLvl1 to listOf(Facet(category3Sub2, 4), Facet(category3Sub1, 2)),
-                hierarchicalCategoryLvl2 to listOf(Facet(category3Sub2Sub1, 1), Facet(category3Sub2Sub2, 3)),
-                hierarchicalCategoryLvl3 to listOf(Facet(category3Sub2Sub2Sub2, 1), Facet(category3Sub2Sub2Sub1, 1))
+                hierarchicalCategoryLvl2 to listOf(Facet(category3Sub2Sub2, 3), Facet(category3Sub2Sub1, 1)),
+                hierarchicalCategoryLvl3 to listOf(Facet(category3Sub2Sub2Sub1, 1), Facet(category3Sub2Sub2Sub2, 1))
             )
             response.hits.size shouldEqual 3
         }
