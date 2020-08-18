dd0ec83a746ffdad34a723b0fccfa30c86aaf564
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-08-18 11:54:05 +0200

    update dependencies: kotlin 1.4.0 and android 4.0.1

--- buildSrc/src/main/kotlin/plugin/Android.kt
+++ buildSrc/src/main/kotlin/plugin/Android.kt
@@ -5,5 +5,5 @@ import Dependency
 object Android : Dependency {
     override val group = "com.android.tools.build"
     override val artifact = "gradle"
-    override val version = "3.6.3"
+    override val version = "4.0.1"
 }
