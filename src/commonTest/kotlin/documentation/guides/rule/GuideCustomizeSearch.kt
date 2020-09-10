585c7434aa08f3491b5f9fd099bba25b9ce6aec6
Mouaad Aallam <mouaad.aallam@algolia.com>
2020-09-10 11:37:22 +0200

    chore(docs): customize search by platform snippet

---
+++ src/commonTest/kotlin/documentation/guides/rule/GuideCustomizeSearch.kt
@@ -0,0 +1,33 @@
+package documentation.guides.rule
+
+import com.algolia.search.client.Index
+import com.algolia.search.model.ObjectID
+import com.algolia.search.model.rule.Condition
+import com.algolia.search.model.rule.Consequence
+import com.algolia.search.model.rule.Rule
+import com.algolia.search.model.search.Query
+import runBlocking
+import kotlin.test.Ignore
+
+@Ignore
+class GuideCustomizeSearch {
+
+    private val movies: Index = TODO()
+
+    /** save contextual **/
+    fun snippet1() {
+        runBlocking {
+            val rule = Rule(
+                objectID = ObjectID("a-rule-id"),
+                conditions = listOf(Condition(
+                    context = "mobile"
+                )),
+                consequence = Consequence(
+                    query = Query(filters = "release_date >= 1577836800")
+                ),
+            )
+
+            movies.saveRule(rule)
+        }
+    }
+}
