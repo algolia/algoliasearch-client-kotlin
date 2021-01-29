0f5ff6b54f73c903b2cb62af3af2ff6047d90795
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-01-29 16:04:44 +0100

    refact: tasks path as constant

--- src/commonMain/kotlin/com/algolia/search/client/internal/ClientSearchImpl.kt
+++ src/commonMain/kotlin/com/algolia/search/client/internal/ClientSearchImpl.kt
@@ -32,6 +32,7 @@ import com.algolia.search.serialize.KeyLength
 import com.algolia.search.serialize.KeyOffset
 import com.algolia.search.serialize.KeyType
 import com.algolia.search.serialize.RouteLogs
+import com.algolia.search.serialize.RouteTask
 import com.algolia.search.transport.RequestOptions
 import com.algolia.search.transport.internal.Transport
 import io.ktor.client.features.ResponseException
@@ -156,8 +157,9 @@ internal class ClientSearchImpl internal constructor(
         return transport.request(
             HttpMethod.Get,
             CallType.Read,
-            "1/task/$taskID",
-            requestOptions)
+            "$RouteTask/$taskID",
+            requestOptions
+        )
     }
 
     /**
