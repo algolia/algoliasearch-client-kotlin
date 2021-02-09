5c9dc65971c23dbc515452ee53b15cf538568add
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-02-09 12:01:09 +0100

    feat: artifacts signing

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,6 +115,7 @@ publishing {
             }
         }
     }
+
     publications.withType<MavenPublication>().all {
         groupId = Library.group
         version = Library.version
@@ -158,4 +159,12 @@ publishing {
         val targetPublication = publications.withType<MavenPublication>().findByName(target.name)
         targetPublication?.artifact(javadocJar)
     }
+}
+
+signing {
+    val signingKeyId = System.getenv("SIGNING_KEY_ID")
+    val signingKey = System.getenv("SIGNING_KEY")
+    val signingPassword = System.getenv("SIGNING_PASSWORD")
+    useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+    sign(publishing.publications)
 }
