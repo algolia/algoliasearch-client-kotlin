ed649fb7066a41f93388a76be1ab9c1a3e424325
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-03-19 18:14:48 +0100

    refact: move the library content to a gradle module

--- algoliasearch-client-kotlin/build/generated/sources/templates/kotlin/main/com.algolia.search.internal/BuildConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.algolia.search.internal
-
-import kotlin.String
-
-internal object BuildConfig {
-    internal const val version: String = "1.7.0"
-}
--- build.gradle.kts
+++ build.gradle.kts
@@ -20,3 +20,7 @@ subprojects {
         mavenCentral()
     }
 }
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
