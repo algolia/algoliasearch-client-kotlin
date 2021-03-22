19b8db887a0f4c2e1307b67cd383dcecfc7cd939
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-03-22 12:18:22 +0100

    refact: gradle modules and release process (#260)

--- buildSrc/src/main/kotlin/Spotless.kt
+++ buildSrc/src/main/kotlin/Spotless.kt
@@ -1,5 +1,3 @@
-package plugin
-
 import Dependency
 
 object Spotless : Dependency {
---
+++ client/build.gradle.kts
@@ -0,0 +1,85 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("com.vanniktech.maven.publish")
+    id("com.diffplug.spotless")
+}
+
+kotlin {
+    explicitApi()
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "1.8"
+        }
+        testRuns["test"].executionTask.configure {
+            useJUnit()
+        }
+    }
+
+    sourceSets {
+        all {
+            languageSettings.apply {
+                progressiveMode = true
+                useExperimentalAnnotation("kotlin.RequiresOptIn")
+            }
+        }
+        val commonMain by getting {
+            kotlin.srcDirs("$buildDir/generated/sources/templates/kotlin/main")
+            dependencies {
+                api(Ktor("client"))
+                api(Ktor("client-json"))
+                api(Ktor("client-logging"))
+                api(Ktor("client-serialization"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+                implementation(Ktor("client"))
+                implementation(Ktor("client-mock"))
+            }
+        }
+        val jvmMain by getting
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation(Ktor("client-okhttp"))
+                implementation(Ktor("client-apache"))
+                implementation(Ktor("client-android"))
+            }
+        }
+    }
+}
+
+tasks {
+
+    withType<KotlinCompile> {
+        dependsOn("copyTemplates")
+    }
+
+    register(name = "copyTemplates", type = Copy::class) {
+        from("src/commonMain/templates")
+        into("$buildDir/generated/sources/templates/kotlin/main")
+        expand("projectVersion" to Library.version)
+        filteringCharset = "UTF-8"
+    }
+
+    withType<Test> {
+        maxParallelForks = Runtime.getRuntime().availableProcessors().minus(1).coerceAtLeast(1)
+    }
+}
+
+configure<SpotlessExtension> {
+    kotlin {
+        target("**/*.kt")
+        ktlint("0.40.0")
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
+
+val javadoc by tasks.creating(Javadoc::class)
