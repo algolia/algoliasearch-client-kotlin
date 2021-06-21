1ea28783bea8ffa4e9a62641e543cdf48f138a96
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-06-21 12:01:06 +0200

    chore: update spotless 5.14.0

--- buildSrc/src/main/kotlin/Spotless.kt
+++ buildSrc/src/main/kotlin/Spotless.kt
@@ -2,5 +2,5 @@ object Spotless : Dependency {
 
     override val group = "com.diffplug.spotless"
     override val artifact = "spotless-plugin-gradle"
-    override val version = "5.12.4"
+    override val version = "5.14.0"
 }
