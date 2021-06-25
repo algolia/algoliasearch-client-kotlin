dc683ed6191314aded3717952c87d9ecf5e9f5c5
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-06-25 16:51:57 +0200

    chore: update kotlin to 1.5.20 and gradle to 7.1 (#268)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlinVersion = "1.5.10"
+        val kotlinVersion = "1.5.20"
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
         classpath(MavenPublish())
