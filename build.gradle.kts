cd398664f80fb041fe85bffbb6be968aae1461ea
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-09-20 17:30:04 +0200

    update maven publish plugin to 0.18.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         val kotlinVersion = "1.5.30"
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
-        classpath("com.vanniktech:gradle-maven-publish-plugin:0.17.0")
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
         classpath("com.diffplug.spotless:spotless-plugin-gradle:5.14.0")
     }
 }
