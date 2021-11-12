eeb6d751c428e0e0abb25f8882299d3db78e308a
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-11-12 16:51:18 +0100

    chore: update kotlin to `1.5.31` (#308)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,12 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlinVersion = "1.5.30"
+        val kotlinVersion = "1.5.31"
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
         classpath("com.diffplug.spotless:spotless-plugin-gradle:5.15.0")
-        classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.7.1")
+        classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.8.0")
     }
 }
 
