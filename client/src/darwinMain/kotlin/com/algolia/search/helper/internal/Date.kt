8355989bd01e206b824406bea886cb5f2601ceb7
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-11-17 17:25:20 +0100

    wip: client date parse

--- client/src/darwinMain/kotlin/com/algolia/search/helper/internal/Date.kt
+++ client/src/darwinMain/kotlin/com/algolia/search/helper/internal/Date.kt
@@ -10,11 +10,12 @@ import platform.Foundation.create
 @ThreadLocal
 internal actual object DateISO8601 {
 
-    private val dateISO8601 = NSISO8601DateFormatter()
+    internal val dateISO8601 = NSISO8601DateFormatter()
 
     @OptIn(UnsafeNumber::class)
-    private val dateISO8601Millis =
-        NSISO8601DateFormatter().apply { setFormatOptions(NSISO8601DateFormatWithFractionalSeconds) }
+    internal val dateISO8601Millis = NSISO8601DateFormatter().apply {
+        setFormatOptions(NSISO8601DateFormatWithFractionalSeconds)
+    }
 
     actual fun format(timestamp: Long, inMilliseconds: Boolean): String {
         val date = NSDate.create(timestamp.toDouble())
@@ -26,5 +27,13 @@ internal actual object DateISO8601 {
         val formatter = if (inMilliseconds) dateISO8601Millis else dateISO8601
         return formatter.dateFromString(date)?.timeIntervalSinceReferenceDate?.toMillis()
             ?: throw IllegalArgumentException("unable to parse $date")
+    }
+
+    fun parse(date: String): NSDate {
+        return when (date.length) {
+            20 -> dateISO8601.dateFromString(date)
+            24 -> dateISO8601Millis.dateFromString(date)
+            else -> NSDate()
+        } ?: throw IllegalArgumentException("unable to parse $date")
     }
 }
--- client/src/darwinMain/kotlin/com/algolia/search/model/ClientDate.kt
+++ client/src/darwinMain/kotlin/com/algolia/search/model/ClientDate.kt
@@ -4,6 +4,7 @@ import com.algolia.search.helper.internal.DateISO8601
 import com.algolia.search.model.internal.Raw
 import com.algolia.search.serialize.KSerializerClientDate
 import kotlinx.serialization.Serializable
+import platform.Foundation.NSDate
 
 /**
  * JVM implementation converting a [String] or a [Long] into a [Date] format. Relies on ISO8601.
@@ -12,4 +13,9 @@ import kotlinx.serialization.Serializable
 public actual data class ClientDate internal actual constructor(override val raw: String) : Raw<String> {
 
     internal actual constructor(timestamp: Long) : this(DateISO8601.format(timestamp, false))
+
+    /**
+     * In the eventuality of the Date format being wrong, we create an empty [NSDate] object instead of throwing an exception.
+     */
+    val date: NSDate = DateISO8601.parse(raw)
 }
