7e36d0f09a872533155b1124c81b2c67ca4ec9c7
Mouaad Aallam <mouaad.aallam@algolia.com>
2021-11-23 11:43:02 +0100

    fix delegation issue for AtomicRef

--- client/src/commonMain/kotlin/com/algolia/search/configuration/RetryableHost.kt
+++ client/src/commonMain/kotlin/com/algolia/search/configuration/RetryableHost.kt
@@ -1,9 +1,5 @@
 package com.algolia.search.configuration
 
-import com.algolia.search.model.internal.Time
-import kotlinx.atomicfu.AtomicRef
-import kotlinx.atomicfu.atomic
-
 public sealed interface RetryableHost {
 
     /** The url to target.*/
@@ -30,41 +26,4 @@ public fun RetryableHost(
     callType: CallType? = null
 ): RetryableHost {
     return AtomicRetryableHost(init = DefaultRetryableHost(url, callType))
-}
-
-internal data class DefaultRetryableHost(
-    override val url: String,
-    override val callType: CallType? = null
-) : RetryableHost {
-
-    override var isUp: Boolean = true
-    override var lastUpdated: Long = Time.getCurrentTimeMillis()
-    override var retryCount: Int = 0
-}
-
-internal class AtomicRetryableHost(
-    private val reference: AtomicRef<DefaultRetryableHost>
-) : RetryableHost by reference.value {
-
-    constructor(init: DefaultRetryableHost) : this(atomic(init))
-
-    fun edit(block: DefaultRetryableHost.() -> Unit) {
-        val tmp = reference.value.copy()
-        block(tmp)
-        reference.value = tmp
-    }
-
-    override fun hashCode(): Int = reference.value.hashCode()
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is AtomicRetryableHost) return false
-        return reference.value == other.reference.value
-    }
-}
-
-internal fun RetryableHost.edit(block: DefaultRetryableHost.() -> Unit) {
-    when (this) {
-        is AtomicRetryableHost -> edit(block)
-        is DefaultRetryableHost -> block(this)
-    }
 }
