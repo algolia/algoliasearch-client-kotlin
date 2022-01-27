9bd0d31bee08feadde80803339629d9022136ae0
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-01-27 16:38:28 +0100

    chore: update to kotlin `1.5.32`

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        val kotlinVersion = "1.6.0"
+        val kotlinVersion = "1.5.32"
         classpath(kotlin("gradle-plugin", version = kotlinVersion))
         classpath(kotlin("serialization", version = kotlinVersion))
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.18.0")
