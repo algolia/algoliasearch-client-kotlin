f8310247edbe530eab1fafa158f6e0e141fe372b
Takahiro Yamada <tyr.wzl@gmail.com>
2022-03-03 21:25:12 +0900

    chore: remove unused import statements

--- client/src/commonMain/kotlin/com/algolia/search/endpoint/internal/EndpointSearch.kt
+++ client/src/commonMain/kotlin/com/algolia/search/endpoint/internal/EndpointSearch.kt
@@ -4,7 +4,6 @@ package com.algolia.search.endpoint.internal
 
 import com.algolia.search.configuration.CallType
 import com.algolia.search.dsl.filters
-import com.algolia.search.dsl.internal.requestOptionsBuilder
 import com.algolia.search.endpoint.EndpointSearch
 import com.algolia.search.model.Attribute
 import com.algolia.search.model.IndexName
@@ -22,7 +21,6 @@ import com.algolia.search.model.search.Cursor
 import com.algolia.search.model.search.Facet
 import com.algolia.search.model.search.FacetStats
 import com.algolia.search.model.search.Query
-import com.algolia.search.serialize.KeyCursor
 import com.algolia.search.serialize.KeyFacetQuery
 import com.algolia.search.serialize.internal.JsonNoDefaults
 import com.algolia.search.serialize.internal.merge
