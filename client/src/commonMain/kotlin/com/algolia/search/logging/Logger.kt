6b21ff9571cea6300288753656038399a2a8aa27
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-05-25 14:21:10 +0200

    make message length limiting multiplatform

--- client/src/commonMain/kotlin/com/algolia/search/logging/Logger.kt
+++ client/src/commonMain/kotlin/com/algolia/search/logging/Logger.kt
@@ -1,5 +1,6 @@
 package com.algolia.search.logging
 
+import com.algolia.search.logging.internal.MessageLengthLimitingLogger
 import com.algolia.search.logging.internal.toLogger
 import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.EMPTY
@@ -21,16 +22,34 @@ public fun interface Logger {
         /**
          * [Logger] using [println].
          */
-        public val Simple: Logger = KLogger.SIMPLE.toLogger()
+        public val Simple: Logger get() = KLogger.SIMPLE.toLogger()
 
         /**
          * Empty [Logger.
          */
-        public val Empty: Logger = KLogger.EMPTY.toLogger()
+        public val Empty: Logger get() = KLogger.EMPTY.toLogger()
 
         /**
          * Default logger to use.
          */
-        public val Default: Logger = KLogger.DEFAULT.toLogger()
+        public val Default: Logger get() = KLogger.DEFAULT.toLogger()
+
+        /**
+         * A [Logger] that breaks up log messages into multiple logs no longer than [maxLength].
+         * Useful for platforms such as Android, which has a log length limited to 4068 characters.
+         *
+         * @property maxLength max length allowed for a log message
+         * @property minLength if log message is longer than [maxLength], attempt to break the log
+         * message at a new line between [minLength] and [maxLength] if one exists
+         */
+        public fun messageLengthLimiting(
+            maxLength: Int = 4000,
+            minLength: Int = 3000,
+            delegate: Logger = Simple
+        ): Logger = MessageLengthLimitingLogger(
+            maxLength = maxLength,
+            minLength = minLength,
+            delegate = delegate
+        )
     }
 }
