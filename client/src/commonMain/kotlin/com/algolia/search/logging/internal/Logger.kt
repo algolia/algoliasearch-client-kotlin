314689be0e7c3e5630cbba850b8726da17942cf5
Mouaad Aallam <mouaad.aallam@algolia.com>
2022-05-27 15:27:41 +0200

    remove unused function

--- client/src/commonMain/kotlin/com/algolia/search/logging/internal/Logger.kt
+++ client/src/commonMain/kotlin/com/algolia/search/logging/internal/Logger.kt
@@ -1,20 +1,13 @@
 package com.algolia.search.logging.internal
 
 import com.algolia.search.logging.Logger
-import io.ktor.client.plugins.logging.Logger as KLogger
+import io.ktor.client.plugins.logging.Logger as KtorLogger
 
 /**
  * Convert [Logger] to a Ktor's Logger.
  */
-internal fun Logger.toKtorLogger() = object : KLogger {
+internal fun Logger.toKtorLogger() = object : KtorLogger {
     override fun log(message: String) {
         this@toKtorLogger.log(message)
     }
-}
-
-/**
- * Convert Ktor's Logger to [Logger]
- */
-internal fun KLogger.toLogger() = Logger { message ->
-    this@toLogger.log(message)
 }
