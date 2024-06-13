2d1923124bfaddc7d34751bf395925378823150e
Thomas Raffray <Fluf22@users.noreply.github.com>
2024-06-13 14:46:51 +0200

    fix: enable omitting order property in FacetValuesOrder

--- client/src/commonMain/kotlin/com/algolia/search/model/rule/FacetValuesOrder.kt
+++ client/src/commonMain/kotlin/com/algolia/search/model/rule/FacetValuesOrder.kt
@@ -1,17 +1,22 @@
 package com.algolia.search.model.rule
 
 import com.algolia.search.serialize.internal.Key
+import kotlinx.serialization.EncodeDefault
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
+@OptIn(ExperimentalSerializationApi::class)
 public data class FacetValuesOrder(
-    /**
-     * Pinned order of facet values.
-     */
-    @SerialName(Key.Order) public val order: List<String> = emptyList(),
     /**
      * How to display the remaining items.
      */
-    @SerialName(Key.SortRemainingBy) public val sortRemainingBy: SortRule? = null
+    @SerialName(Key.SortRemainingBy) public val sortRemainingBy: SortRule? = null,
+    /**
+     * Pinned order of facet values.
+     */
+    @SerialName(Key.Order)
+    @EncodeDefault(EncodeDefault.Mode.NEVER)
+    public val order: List<String>? = null
 )
